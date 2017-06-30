9b1bd16faefc6a0eebbc82b77de82f4e8c6522b6
Mühlhaus <muehlhaus@bio.uni-kl.de>
2017-06-30 16:46:21 +0200

    Add quantile estimation

---
+++ src/FSharp.Stats/Quantile.fs
@@ -0,0 +1,331 @@
+﻿namespace FSharp.Stats
+
+
+/// Module to estimate different quantile measures
+module Quantile =
+
+    type QuantileFunction<'a> =  float -> array<'a> -> float
+
+    let private quantileHelper (quatileF : QuantileFunction<'a>) q (data:array<'a>) =
+
+        if (q < 0. || q > 1. || data.Length = 0) then
+            nan
+        elif (q = 0. || data.Length = 1) then
+            Array.min data |> float
+        elif (q = 1.) then
+            Array.max data |> float
+        else            
+            quatileF q data
+
+
+    /// ! Works inplace and can thus causes the data array to be reordered
+    module InPlace =
+
+        
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        /// Approximately median-unbiased regardless of the sample distribution.
+        let computeInplace q (data:array<_>) =
+        
+            let h  = ((float data.Length + 1./3.)*q + 1./3.)
+            let h' = h |> int
+        
+            if (q < 0. || q > 1. || data.Length = 0) then
+                nan
+            elif (h' <= 0 || q = 0.) then
+                Array.min data
+            elif (h' >= data.Length || q = 1.) then
+                Array.max data
+            else
+                let a = Array.quickSelectInPlace (h'-1) data
+                let b = Array.quickSelectInPlace h' data
+                a + (h - float h') * (b - a);    
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let empiricalInvCdfInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h = float data.Length * q + 0.5
+                Array.quickSelectInPlace (int (ceil (h-0.5))-1) data
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let empiricalInvCdfAverageInPLace q (data:array<_>) =
+
+            let f q (data:array<_>) =
+                let h = float data.Length * q + 0.5
+                let a = Array.quickSelectInPlace (int (ceil (h - 0.5)) - 1) data
+                let b = Array.quickSelectInPlace (int (((h + 0.5) - 1.)*0.5)) data
+                a + b
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let nearestInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h = float data.Length * q
+                Array.quickSelectInPlace (int (System.Math.Round(h)) - 1) data
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let californiaInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h  = float data.Length * q
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let hazenInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h  = float data.Length * q + 0.5
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data        
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let nistInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h  = float (data.Length+1) * q
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let modeInPLace q (data:array<_>) =
+            let f q (data:array<_>) =                
+                let h  = float (data.Length+1) * q + 1.
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let inline normalInPLace q (data:array<_>) =
+            let f q (data:array<'a>) =                
+                let h  = (float data.Length + 0.25) * q + 0.375
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data |> float
+                let b = Array.quickSelectInPlace (h') data |> float
+                a - (h - float h') * (b - a)
+            
+            quantileHelper f q data
+
+
+    /// ! Input needs to be sorted
+    module OfSorted =
+
+        
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        /// Approximately median-unbiased regardless of the sample distribution.
+        let computeOfSorted q (data:array<_>) =
+        
+            let h  = ((float data.Length + 1./3.)*q + 1./3.)
+            let h' = h |> int
+        
+            if (q < 0. || q > 1. || data.Length = 0) then
+                nan
+            elif (h' <= 0 || q = 0.) then
+                Array.min data
+            elif (h' >= data.Length || q = 1.) then
+                Array.max data
+            else
+                let a = Array.quickSelectInPlace (h'-1) data
+                let b = Array.quickSelectInPlace h' data
+                a + (h - float h') * (b - a);    
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let empiricalInvCdfInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h = float data.Length * q + 0.5
+                Array.quickSelectInPlace (int (ceil (h-0.5))-1) data
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let empiricalInvCdfAverageInPLace q (data:array<_>) =
+
+            let f q (data:array<_>) =
+                let h = float data.Length * q + 0.5
+                let a = Array.quickSelectInPlace (int (ceil (h - 0.5)) - 1) data
+                let b = Array.quickSelectInPlace (int (((h + 0.5) - 1.)*0.5)) data
+                a + b
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let nearestInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h = float data.Length * q
+                Array.quickSelectInPlace (int (System.Math.Round(h)) - 1) data
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let californiaInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h  = float data.Length * q
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let hazenInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h  = float data.Length * q + 0.5
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data        
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let nistInPLace q (data:array<_>) =
+            let f q (data:array<_>) =
+                let h  = float (data.Length+1) * q
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let modeInPLace q (data:array<_>) =
+            let f q (data:array<_>) =                
+                let h  = float (data.Length+1) * q + 1.
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data
+                let b = Array.quickSelectInPlace (h') data
+                a - (h - float h') * (b - a)
+        
+            quantileHelper f q data
+
+
+        /// Estimates the q-th quantile from the unsorted data array. (in place)
+        let inline normalInPLace q (data:array<_>) =
+            let f q (data:array<'a>) =                
+                let h  = (float data.Length + 0.25) * q + 0.375
+                let h' = int h
+                let a = Array.quickSelectInPlace (h' - 1) data |> float
+                let b = Array.quickSelectInPlace (h') data |> float
+                a - (h - float h') * (b - a)
+            
+            quantileHelper f q data
+
+
+
+    // ++++++++++++++++++++++++++++++++++++
+
+    /// Estimates the q-th quantile from the unsorted data array.
+    /// Approximately median-unbiased regardless of the sample distribution.
+    let compute q (data:array<_>) =
+        
+        let h  = ((float data.Length + 1./3.)*q + 1./3.)
+        let h' = h |> int
+        
+        if (q < 0. || q > 1. || data.Length = 0) then
+            nan
+        elif (h' <= 0 || q = 0.) then
+            Array.min data
+        elif (h' >= data.Length || q = 1.) then
+            Array.max data
+        else
+            let data' = Array.copy data
+            let a = Array.quickSelectInPlace (h'-1) data'
+            let b = Array.quickSelectInPlace h' data'
+            a + (h - float h') * (b - a); 
+
+
+    /// Estimates the q-th quantile from the unsorted data array.
+    let empiricalInvCdf q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.empiricalInvCdfInPLace q data'
+    
+    
+    /// Estimates the q-th quantile from the unsorted data array.
+    let empiricalInvCdfAverage q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.empiricalInvCdfAverageInPLace q data'        
+
+
+    /// Estimates the q-th quantile from the unsorted data array.
+    let nearest q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.nearestInPLace q data'   
+
+
+    /// Estimates the q-th quantile from the unsorted data array.
+    let empiricalInvCdfInPLace q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.empiricalInvCdfInPLace q data'
+        
+     
+    /// Estimates the q-th quantile from the unsorted data array.
+    let california q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.californiaInPLace q data'
+    
+    
+    /// Estimates the q-th quantile from the unsorted data array.
+    let hazen q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.hazenInPLace q data'        
+
+
+    /// Estimates the q-th quantile from the unsorted data array.
+    let nist q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.nistInPLace q data'
+    
+    
+    /// Estimates the q-th quantile from the unsorted data array.
+    let mode q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.modeInPLace q data'        
+    
+    
+    /// Estimates the q-th quantile from the unsorted data array.
+    let normal q (data:array<_>) =
+        let data' = Array.copy data
+        InPlace.normalInPLace q data'
+
+
+
+    /// Computes the interquartile range (IQR)
+    //  The IQR is the 1st Quartile subtracted from the 3rd Quartile; these quartiles can be clearly seen on a box plot on the data.
+    //  It is a trimmed estimator, defined as the 25% trimmed mid-range, and is the most significant basic robust measure of scale.
+    let interQuantileRange (qf:QuantileFunction<'a>) (data:array<'a>) =
+        (qf 0.75 data) - (qf 0.25 data)
+        
+
--- src/FSharp.Stats/Testing/PvalueAdjust.fs
+++ src/FSharp.Stats/Testing/PvalueAdjust.fs
@@ -27,7 +27,111 @@ module PvalueAdjust =
 
 
 
-
+    module Qvalue = 
+        
+        let a = nan 
+ 
+//        //let lambda = [|0.0 ..0.05..0.9|]
+//        let pi0_Bootstrap (lambda:float[]) (p:float[])  =
+//            let pi0 = Array.init lambda.Length ( fun i -> ( (Descriptive.StatisticalMeasure.mean((p |> Array.map (fun v -> if (v >= lambda.[i]) then 1. else 0. ))) ) / (1. - lambda.[i]) ) )
+//            let minpi0 = Array.min(pi0)    
+//
+//            let rnd = System.Random()
+//
+//            let rec floop (counter:int) (result:float[]) =    
+//                if counter > 0 then
+//                    let pboot    = Fitting.Bootstrap.sampleWithReplacement rnd p p.Length            
+//                    let pi0boot = Array.init lambda.Length ( fun i -> ( (Descriptive.StatisticalMeasure.mean(pboot |> Array.map (fun v -> if (v >= lambda.[i]) then 1. else 0. ))) ) / (1. - lambda.[i]) ) 
+//                    let mse = Array.map2 (fun m p -> m + ((p - minpi0)**2.0) ) result pi0boot
+//                    floop (counter - 1) mse
+//                else
+//                    result
+//
+//            let mse = floop (100) (Array.zeroCreate (lambda.Length))    
+//            let tmp = pi0.[(mse |> Array.findIndex(fun v -> v = (Array.min mse)))]
+//            min (tmp) (1.0)
+//
+//
+//        let pValueOfQValue (qvalues:float[]) =
+//            // estimate of pi0 used to create the q-value that's the maximum q-value (or very, very close to it)
+//            let pi0 = Array.max qvalues
+//            // m0, the estimated number of true nulls
+//            let m0 = float qvalues.Length * pi0
+//            qvalues
+//            |> Statistics.Descriptive.Rank.breakByMean
+//            |> Seq.sortBy (fun r -> r.Position)
+//            // multiply each q-value by the proportion of true nulls expected to be under it (the inverse of how you get there from the p-value)
+//            |> Seq.map (fun r -> r.Value * r.RankIndex / m0)    
+//            |> Array.ofSeq
+//    
+//
+//
+//        let qValueOfPValue (pi0:float) (pvalues:float[]) =        
+//            let m0 = float pvalues.Length * pi0
+//            pvalues
+//            |> Statistics.Descriptive.Rank.breakByMean
+//            |> Seq.sortBy (fun r -> r.Position)    
+//            |> Seq.map (fun r -> r.Value / r.RankIndex * m0)    
+//            |> Array.ofSeq
+//    
+//
+//
+//
+//
+//
+//
+//        let inline checkNonNull argName arg = 
+//            match box arg with 
+//            | null -> nullArg argName 
+//            | _ -> ()
+//
+//
+//        let pairwiseWithLast (source: seq<'T>) =
+//            checkNonNull "source" source
+//            seq { use ie = source.GetEnumerator() 
+//                  if ie.MoveNext() then
+//                        let iref = ref ie.Current
+//                        while ie.MoveNext() do
+//                            let j = ie.Current 
+//                            yield (!iref, j)
+//                            iref := j 
+//                        yield (!iref,!iref)}
+//
+//
+//        let qValueOfPValue' (pi0:float) (pvalues:float[]) =        
+//            let m0 = float pvalues.Length * pi0
+//            pvalues
+//            |> Statistics.Descriptive.Rank.breakByMean
+//            |> Seq.map (fun r -> let v =  r.Value / r.RankIndex * m0
+//                                 {r with Value = v}
+//                        )
+//            |> pairwiseWithLast
+//            |> Seq.map (fun (f,s) -> let v = min (min f.Value s.Value) 1.
+//                                     {f with Value = v}
+//                        )
+//            |> Seq.sortBy (fun r -> r.Position)
+//            |> Seq.map (fun r -> r.Value)
+//            |> Array.ofSeq
+//
+//
+//
+//    
+//        //// Example
+//        //let pvalues = Seq.fromFile "D:\\output.txt" |> Seq.map float |> Seq.toArray
+//        //
+//        //let pi0 = pi0_Bootstrap [|0.0 ..0.05..0.9|] pvalues
+//        //
+//        //let qvalues  = qValueOfPValue pi0 pvalues
+//        //let qvalues' = qValueOfPValue' pi0 pvalues
+//        //
+//        //
+//        //let bw = Statistics.Fitting.Bandwidth.nrd0 pvalues
+//        //let histo = Descriptive.Histogram.create (bw/2.) pvalues |> Descriptive.EmpiricalPMF.ofHistogram |> Descriptive.EmpiricalPMF.getZip |> Chart.Point |> Chart.ShowChart
+//        //
+//        //
+//        //
+//        //Chart.Point(Seq.zip pvalues qvalues ) |> Chart.ShowChart
+//        //   
 
 
 
