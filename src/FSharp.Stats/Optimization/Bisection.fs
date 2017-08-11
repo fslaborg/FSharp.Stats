e33b70d346c9e34a145bea968f48fb11e75f4cdf
HLWeil <hl.weil@t-online.de>
2017-08-11 22:03:18 +0200

    add bisection

---
+++ src/FSharp.Stats/Optimization/Bisection.fs
@@ -0,0 +1,39 @@
+﻿namespace FSharp.Stats.Optimization
+
+module Bisection = 
+    
+    ///Finds the value in an interval for which a given function returns a value at least as close to 0 as the accuracy 
+    let tryFindRoot func accuracy lowerBound upperBound maxIter = 
+
+        let rec loop a b i = 
+            let c = (a + b)/2.
+            let fc = func c
+            if fc = 0. then 
+                Some c
+            elif (b - a)/2. < accuracy then 
+                Some c
+            else
+                if i = maxIter then None
+                else 
+                    if sign fc = sign (func a) then loop c b (i+1)
+                    else loop a c (i+1)
+
+        let checkConditions a b = 
+            let fa = func a
+            let fb = func b
+            if fa = 0. then Some a
+            elif fb = 0. then Some b
+            else 
+                if fa < 0. then 
+                    if fb > 0. then
+                        loop a b 0
+                    else None
+                else 
+                    if fb < 0. then 
+                        loop a b 0
+                    else None
+
+        if lowerBound < upperBound then checkConditions lowerBound upperBound
+        else checkConditions upperBound lowerBound
+            
+
