e9e81744b47bd944d8b4a3be93d2f4adac15ad2d
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2017-10-17 13:25:53 +0200

    Fixed SummeryStats

--- src/FSharp.Stats/RunningStats.fs
+++ src/FSharp.Stats/RunningStats.fs
@@ -7,23 +7,23 @@ module SummeryStats =
     type SummeryStats<'T> = {
         N           : 'T
         Mean        : 'T
-        SumOfSqures : 'T
+        SumOfSquares : 'T
         Min         : 'T
         Max         : 'T
     }
 
     let createSummeryStats n mean sos min max =
-        {N=n;Mean=mean;SumOfSqures=sos;Min=min;Max=max}
+        {N=n;Mean=mean;SumOfSquares=sos;Min=min;Max=max}
 
     
     ///
     let inline mean sStats = sStats.Mean
     ///
-    let inline varPopulation sStats = sStats.SumOfSqures / sStats.N
+    let inline varPopulation sStats = sStats.SumOfSquares / sStats.N
     ///
     let inline var (sStats:SummeryStats<'T>) = 
         let one = LanguagePrimitives.GenericOne<'T>
-        sStats.SumOfSqures / (sStats.N - one)
+        sStats.SumOfSquares / (sStats.N - one)
 
 
 module RunningStats =
