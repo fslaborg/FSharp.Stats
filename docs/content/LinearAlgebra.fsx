939aa7e07407b740962221580013b6f705c7d3ed
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2018-01-07 11:55:59 +0100

    Add Savitzky-Golay filtering

--- docs/content/LinearAlgebra.fsx
+++ docs/content/LinearAlgebra.fsx
@@ -35,61 +35,31 @@ let B =
 
 let P,L,U = Algebra.LinearAlgebraManaged.LU A
 
-open Algebra.LinearAlgebraManaged
 
-let matrixDims (m:Matrix<_>) = m.NumRows, m.NumCols
+open FSharp.Stats.Algebra
+open FSharp.Stats.Algebra.LinearAlgebraManaged
 
-/// Calculates the pseudo inverse of the matrix
-let pseudoInvers (matrix:Matrix<float>) =
-    let (m,n) = matrixDims matrix
-    // Is this an overdetermined or underdetermined system?
-    if m > n then
-        let qm,R = QR matrix
-        let i = Matrix.identity m
-        let Qtb = qm.Transpose * i
-        SolveTriangularLinearSystems R.[0..n-1,0..n-1] Qtb.[0..n-1,0..m-1] false
-    else
-        let qm,R = QR matrix.Transpose
-        let i = Matrix.identity n
-        let Qtb = qm.Transpose * i        
-        let s = SolveTriangularLinearSystems R.[0..m-1,0..m-1] Qtb.[0..m-1,0..n-1] false
-        s.Transpose
+let SolveLinearSystemsByQR (A:matrix) (B:matrix) =
+    let (n,m) = A.Dimensions
+    //if n <> m then invalidArg "Matrix" "Matrix must be square." 
+    let q,r = QR A
+    let y = q.Transpose * B
+    (SolveTriangularLinearSystems r y true)
 
 
-let im =
-       [[1.0; -15.0; 225.0; -3375.0; 50625.0]
-        [1.0; -14.0; 196.0; -2744.0; 38416.0]
-        [1.0; -13.0; 169.0; -2197.0; 28561.0]
-        [1.0; -12.0; 144.0; -1728.0; 20736.0]
-        [1.0; -11.0; 121.0; -1331.0; 14641.0]
-        [1.0; -10.0; 100.0; -1000.0; 10000.0]
-        [1.0; -9.0; 81.0; -729.0; 6561.0]
-        [1.0; -8.0; 64.0; -512.0; 4096.0]
-        [1.0; -7.0; 49.0; -343.0; 2401.0]
-        [1.0; -6.0; 36.0; -216.0; 1296.0]
-        [1.0; -5.0; 25.0; -125.0; 625.0]
-        [1.0; -4.0; 16.0; -64.0; 256.0]
-        [1.0; -3.0; 9.0; -27.0; 81.0]
-        [1.0; -2.0; 4.0; -8.0; 16.0]
-        [1.0; -1.0; 1.0; -1.0; 1.0]
-        [1.0; 0.0; 0.0; 0.0; 0.0]
-        [1.0; 1.0; 1.0; 1.0; 1.0]
-        [1.0; 2.0; 4.0; 8.0; 16.0]
-        [1.0; 3.0; 9.0; 27.0; 81.0]
-        [1.0; 4.0; 16.0; 64.0; 256.0]
-        [1.0; 5.0; 25.0; 125.0; 625.0]
-        [1.0; 6.0; 36.0; 216.0; 1296.0]
-        [1.0; 7.0; 49.0; 343.0; 2401.0]
-        [1.0; 8.0; 64.0; 512.0; 4096.0]
-        [1.0; 9.0; 81.0; 729.0; 6561.0]
-        [1.0; 10.0; 100.0; 1000.0; 10000.0]
-        [1.0; 11.0; 121.0; 1331.0; 14641.0]
-        [1.0; 12.0; 144.0; 1728.0; 20736.0]
-        [1.0; 13.0; 169.0; 2197.0; 28561.0]
-        [1.0; 14.0; 196.0; 2744.0; 38416.0]
-        [1.0; 15.0; 225.0; 3375.0; 50625.0]]
-        |> matrix
+let A = 
+    matrix [ [ 2.0; 1.0; 1.0 ]
+             [ 1.0; 3.0; 2.0 ]
+             [ 1.0; 0.0; 0.0 ] ]
 
 
-pseudoInvers im
+
+let B = 
+    matrix [ [ 4.0; ]
+             [ 5.0; ]
+             [ 6.0; ] ]
+
+// [  6.  15. -23.]
+SolveLinearSystems A B
+
 
--- docs/content/Signal.fsx
+++ docs/content/Signal.fsx
@@ -2,7 +2,7 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
-#r "../../packages/build/FSharp.Plotly/lib/net40/Fsharp.Plotly.dll"
+#r "../../packages/build/FSharp.Plotly/lib/net45/Fsharp.Plotly.dll"
 open FSharp.Plotly
 (**
 
@@ -34,35 +34,71 @@ open FSharp.Plotly
 #r "FSharp.Stats.dll"
 open FSharp.Stats
 
-let fs = 1000            // Sampling frequency                    
-let tp = 1. / float fs    // Sampling period       
-let l = 1500;            // Length of signal
-// Time vector
-let t = Array.init (l-1) (fun x -> float x * tp)       
+//// Fast fourier transform
 
-let pi = System.Math.PI
+//let fs = 1000            // Sampling frequency                    
+//let tp = 1. / float fs    // Sampling period       
+//let l = 1500;            // Length of signal
+//// Time vector
+//let t = Array.init (l-1) (fun x -> float x * tp)       
 
-let signal t = 0.7 * sin (2.*pi*50.*t) + sin (2.*pi*120.*t)
-let y = t |> Array.map signal
-let y' = Signal.FFT.inverse (y |> Array.map (fun v ->  Complex.Create (v, 0.) )) |> Array.map (fun c -> c.RealPart)
+//let pi = System.Math.PI
+
+//let signal t = 0.7 * sin (2.*pi*50.*t) + sin (2.*pi*120.*t)
+//let y = t |> Array.map signal
+//let y' = Signal.FFT.inverseInPlace (y |> Array.map (fun v ->  Complex.Create (v, 0.) )) |> Array.map (fun c -> c.RealPart)
+
+//[
+//    Chart.Line(t,y)
+//    Chart.Line(t,y')
+//]
+//|> Chart.Combine
+//|> Chart.Show
+
+
+//Chart.Line(t,y') |> Chart.Show
+
+
+ 
+
+
+// Savitzky-Golay low-pass filtering
+
+let t  = [|-4. ..(8.0/500.).. 4.|]
+let dy  = t |> Array.map (fun t -> (-t**2.) + (Distributions.Continuous.Normal.Sample 0. 0.5) )
+let dy' = t |> Array.map (fun t -> (-t**2.))
+
+
+let dysg = Signal.Filtering.savitzky_golay  31 4 0 1 dy
+
 
 [
-    Chart.Line(t,y)
-    Chart.Line(t,y')
+    Chart.Point(t, dy, Name="data with noise");
+    Chart.Point(t, dy', Name="data without noise");
+    Chart.Point(t, dysg, Name="data sg");
 ]
 |> Chart.Combine
 |> Chart.Show
 
 
-Chart.Line(t,y') |> Chart.Show
 
 
 
-let x = [|0. .. 0.1 .. 10.|]
-let f t = 5. + 2. * cos (2.*pi*t-90.) + 3.* cos (4.*pi*t)
-let y = x |> Array.map f
-let y' = Signal.FFT.inverse (y |> Array.map (fun v ->  Complex.Create (v, 0.) )) |> Array.map (fun c -> c.RealPart)
 
-Chart.Line(x,x |> Array.map f) |> Chart.Show
 
-Chart.Line(x,y') |> Chart.Show
+
+
+
+
+
+//let x = [|0. .. 0.1 .. 10.|]
+//let f t = 5. + 2. * cos (2.*pi*t-90.) + 3.* cos (4.*pi*t)
+//let y = x |> Array.map fA
+//let y' = Signal.FFT.inverseInPlace (y |> Array.map (fun v ->  Complex.Create (v, 0.) )) |> Array.map (fun c -> c.RealPart)
+
+//Chart.Line(x,x |> Array.map f) |> Chart.Show
+
+//Chart.Line(x,y') |> Chart.Show
+
+
+
