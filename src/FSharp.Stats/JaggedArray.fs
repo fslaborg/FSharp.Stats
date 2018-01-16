97a8aca777f58fc21c691b07fcf49edf79357332
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2018-01-16 19:21:18 +0100

    Add SAM

---
+++ src/FSharp.Stats/JaggedArray.fs
@@ -0,0 +1,228 @@
+﻿namespace FSharp.Stats
+
+[<AutoOpen>]
+module JaggedArray =
+
+    /// Copies the jagged array
+    let copy (arr : _[][]) = 
+        Array.init arr.Length (fun i ->
+            Array.copy arr.[i]
+            )    
+
+    /// Transposes a jagged array
+    let transpose (arr: 'T [][]) =
+        if arr.Length > 0 then 
+            let colSize = arr.[0].Length
+            Array.init (colSize) (fun rowI ->  Array.init (arr.Length) (fun colI -> (arr.[colI].[rowI])))
+        else
+            arr
+
+    /// Converts a jagged list into a jagged array
+    let ofJaggedList (data: 'T list list) =
+        data
+        |> List.map (fun l -> l |> Array.ofList)
+        |> Array.ofList
+
+    /// Converts a jagged array into a jagged list
+    let toJaggedList (arr: 'T [][]) =
+        arr
+        |> Array.map (fun a -> a |> List.ofArray)
+        |> List.ofArray
+
+    /// Converts a jagged Seq into a jagged array
+    let ofJaggedSeq (data: seq<#seq<'T>>) =
+        data
+        |> Seq.map (fun s -> s |> Array.ofSeq)
+        |> Array.ofSeq
+
+    /// Converts a jagged array into a jagged seq
+    let toJaggedSeq (arr: 'T [][]) =
+        arr
+        |> Seq.map (fun s -> s |> Array.toSeq) 
+
+    /// Builds a new jagged array whose inner arrays are the results of applying the given function to each of their elements.
+    let map (mapping: 'T -> 'U) (jArray : 'T[][]) =
+        jArray
+        |> Array.map (fun x -> x |> Array.map mapping)
+
+    /// Builds a new jagged array whose inner arrays are the results of applying the given function to the corresponding elements of the inner arrays of the two jagged arrays pairwise. 
+    /// All corresponding inner arrays must be of the same length, otherwise ArgumentException is raised.
+    let map2 (mapping: 'T1 -> 'T2 -> 'U) (jArray1 : 'T1[][]) (jArray2 : 'T2[][]) = 
+        jArray1
+        |> Array.mapi (fun index x -> (Array.map2 mapping x jArray2.[index]))
+
+    /// Builds a new jagged array whose inner arrays are the results of applying the given function to the corresponding elements of the inner arrays of the tree jagged arrays triplewise. 
+    /// All corresponding inner arrays must be of the same length, otherwise ArgumentException is raised.
+    let map3 (mapping : 'T1 -> 'T2 -> 'T3 -> 'U) (jArray1 : 'T1[][]) (jArray2 : 'T2[][]) (jArray3 : 'T3[][]) =
+        jArray1 |> Array.mapi (fun index x -> (Array.map3 mapping x jArray2.[index] jArray3.[index]))
+
+    ///Builds a new jagged array whose inner arrays are the results of applying the given function to each of their elements. The integer index passed to the function indicates the index of element in the inner array being transformed.
+    let mapi (mapping: int -> 'T -> 'U) (jArray : 'T[][]) =
+        jArray
+        |> Array.map (fun x -> x |> Array.mapi mapping)
+
+    ///Applies a function to each element of the inner arrays of the jagged array, threading an accumulator argument through the computation.
+    let innerFold (folder: 'State -> 'T -> 'State) (state: 'State) (jArray : 'T[][]) =
+        jArray
+        |> Array.map (fun x -> x |> Array.fold folder state )
+
+    ///Applies a function to each element of the inner arrays of the jagged array, threading an accumulator argument through the computation. 
+    ///A second function is the applied to each result of the predeceding computation, again passing an accumulater through the computation 
+    let fold (innerFolder : 'State1 -> 'T -> 'State1) (outerFolder : 'State2 -> 'State1 -> 'State2) (innerState : 'State1) (outerState : 'State2) ((jArray : 'T[][])) =
+        jArray
+        |> innerFold innerFolder innerState
+        |> Array.fold outerFolder outerState
+
+    ///Returns a new jagged array whose inner arrays only contain the elements for which the given predicate returns true
+    let innerFilter (predicate: 'T -> bool) (jArray: 'T[][]) =
+        jArray
+        |> Array.map (fun x -> x |> Array.filter predicate)
+
+    ///Applies the given function to each element in the inner arrays of the jagged array. Returns the jagged array whose inner arrays are comprised of the results x for each element where the function returns Some(x)
+    let innerChoose (chooser: 'T -> 'U option) (jArray: 'T[][]) =
+        jArray
+        |> Array.map (fun x -> x |> Array.choose chooser)
+
+
+    /// Shuffles each column of a jagged array separately  (method: Fisher-Yates)
+    let shuffleColumnWiseInPlace (arr: 'T [][]) =
+        if arr.Length > 0 then 
+            let random      = new System.Random()
+            let rowCount    = arr.Length
+            let columnCount = arr.[0].Length
+            
+            for ci = columnCount - 1 downto 0 do 
+                for ri = rowCount downto  1 do
+                    // Pick random element to swap.
+                    let rj = random.Next(ri) // 0 <= j <= i-1
+                    // Swap.
+                    let tmp         =  arr.[rj].[ci]
+                    arr.[rj].[ci]     <- arr.[ri - 1].[ci]
+                    arr.[ri - 1].[ci] <- tmp
+            arr            
+
+        else
+            arr
+
+
+    /// Shuffles each row of a jagged array separately  (method: Fisher-Yates)
+    let shuffleRowWiseInPlace (arr: 'T [][]) =
+        if arr.Length > 0 then 
+            let random      = new System.Random()
+            let rowCount    = arr.Length
+            let columnCount = arr.[0].Length
+            
+            for ri = rowCount - 1 downto  0 do
+                for ci = columnCount downto 1 do 
+                    // Pick random element to swap.
+                    let cj = random.Next(ci) // 0 <= j <= i-1
+                    // Swap.
+                    let tmp           =  arr.[ri].[cj]
+                    arr.[ri].[cj]     <- arr.[ri].[ci - 1]
+                    arr.[ri].[ci - 1] <- tmp
+            arr            
+
+        else
+            arr
+
+
+    /// Shuffels a jagged array (method: Fisher-Yates)
+    let shuffleInPlace (arr: 'T [][]) =
+        if arr.Length > 0 then 
+            let random      = new System.Random()
+            let rowCount    = arr.Length
+            let columnCount = arr.[0].Length
+            for ri = rowCount downto 1 do
+                for ci = columnCount downto 1 do 
+                    // Pick random element to swap.
+                    let rj = random.Next(ri) // 0 <= j <= i-1
+                    let cj = random.Next(ci)
+                    // Swap.
+                    let tmp               =  arr.[rj].[cj]
+                    arr.[rj].[cj]         <- arr.[ri - 1].[ci - 1]
+                    arr.[ri - 1].[ci - 1] <- tmp
+            arr            
+
+        else
+            arr
+
+[<AutoOpen>]
+module JaggedList =
+    
+
+    // Transposes a jagged array
+    let transpose (data: 'T list list) =
+        let rec transpose = function
+            | (_::_)::_ as M -> List.map List.head M :: transpose (List.map List.tail M)
+            | _ -> []
+        transpose data
+
+
+    // Converts a jagged list into a jagged array     
+    let toJaggedList (data: 'T list list) =
+        data
+        |> List.map (fun l -> l |> Array.ofList)
+        |> Array.ofList
+
+
+    // Converts a jagged array into a jagged list
+    let ofJaggedArray (arr: 'T [][]) =
+        arr
+        |> Array.map (fun a -> a |> List.ofArray)
+        |> List.ofArray
+
+    // Converts a jagged Seq into a jagged list
+    let ofJaggedSeq (data: seq<#seq<'T>>) =
+        data
+        |> Seq.map (fun s -> s |> List.ofSeq)
+        |> List.ofSeq
+
+    // Converts a jagged list into a jagged seq
+    let toJaggedSeq (data: 'T list list) =
+        data
+        |> Seq.map (fun s -> s |> List.toSeq) 
+
+    /// Builds a new jagged list whose inner lists are the results of applying the given function to each of their elements.
+    let map (mapping: 'T -> 'U) (jlist : 'T list list) =
+        jlist
+        |> List.map (fun x -> x |> List.map mapping)
+
+    /// Builds a new jagged list whose inner lists are the results of applying the given function to the corresponding elements of the inner lists of the two jagged lists pairwise. 
+    /// All corresponding inner lists must be of the same length, otherwise ArgumentException is raised.
+    let map2 (mapping: 'T1 -> 'T2 -> 'U) (jlist1 : 'T1 list list) (jlist2 : 'T2 list list) = 
+        jlist1
+        |> List.mapi (fun index x -> (List.map2 mapping x jlist2.[index]))
+
+    /// Builds a new jagged list whose inner lists are the results of applying the given function to the corresponding elements of the inner lists of the tree jagged lists triplewise. 
+    /// All corresponding inner lists must be of the same length, otherwise ArgumentException is raised.
+    let map3 (mapping : 'T1 -> 'T2 -> 'T3 -> 'U) (jlist1 : 'T1 list list) (jlist2 : 'T2 list list) (jlist3 : 'T3 list list) =
+        jlist1 |> List.mapi (fun index x -> (List.map3 mapping x jlist2.[index] jlist3.[index]))
+
+    ///Builds a new jagged list whose inner lists are the results of applying the given function to each of their elements. The integer index passed to the function indicates the index of element in the inner list being transformed.
+    let mapi (mapping: int -> 'T -> 'U) (jlist : 'T list list) =
+        jlist
+        |> List.map (fun x -> x |> List.mapi mapping)
+
+    ///Applies a function to each element of the inner lists of the jagged list, threading an accumulator argument through the computation.
+    let innerFold (folder: 'State -> 'T -> 'State) (state: 'State) (jlist : 'T list list) =
+        jlist
+        |> List.map (fun x -> x |> List.fold folder state )
+
+    ///Applies a function to each element of the inner lists of the jagged list, threading an accumulator argument through the computation. 
+    ///A second function is the applied to each result of the predeceding computation, again passing an accumulater through the computation 
+    let fold (innerFolder : 'State1 -> 'T -> 'State1) (outerFolder : 'State2 -> 'State1 -> 'State2) (innerState : 'State1) (outerState : 'State2) ((jlist : 'T list list)) =
+        jlist
+        |> innerFold innerFolder innerState
+        |> List.fold outerFolder outerState
+
+    ///Returns a new jagged list whose inner lists only contain the elements for which the given predicate returns true
+    let innerFilter (predicate: 'T -> bool) (jlist: 'T list list) =
+        jlist
+        |> List.map (fun x -> x |> List.filter predicate)
+
+    ///Applies the given function to each element in the inner lists of the jagged List. Returns the jagged list whose inner lists are comprised of the results x for each element where the function returns Some(x)
+    let innerChoose (chooser: 'T -> 'U option) (jlist: 'T list list) =
+        jlist
+        |> List.map (fun x -> x |> List.choose chooser)
+
+
--- src/FSharp.Stats/Testing/SAM.fs
+++ src/FSharp.Stats/Testing/SAM.fs
@@ -4,52 +4,141 @@ open FSharp.Stats
 
 module SAM = 
 
+    type SAM = {
+        /// relative diffence of mean
+        Ri : float
+        /// pooled standard error 
+        Si : float
+        /// test statistics
+        Statistics : float
+        }
+
+    let createSAM ri si stats =
+        {Ri=ri; Si=si; Statistics=stats}
+
+
+    /// Calculates SAM test statistics 
+    let calculate s0 (dataA:float array array) (dataU:float array array) =
+    
+        let calcStats s0 (a:float[]) (u:float[]) =        
+            let ma,mu = Array.average a,Array.average u
+            let ri    = ma - mu
+            let si    = 
+                let n = ((1.0/ float a.Length) + (1.0/ float u.Length)) / float (a.Length+u.Length-2 )
+                n * (a |> Array.sumBy (fun v -> v - ma * v - ma)) + (u |> Array.sumBy (fun v -> v - mu * v - mu))
+                |> sqrt
+
+            createSAM ri si (ri/(si+s0))
+
+        Array.map2 (fun a u -> calcStats s0 a u) dataA dataU    
+
+
+    /// 
+    let permutationBalancedColWiseSAM iterations s0 (dataA:float array array) (dataU:float array array) =
+        //let rnd = System.Random()
+        let dataA' = JaggedArray.copy dataA
+        let dataU' = JaggedArray.copy dataU
+    
+        [|0..iterations-1|]
+        |> Array.map (fun _ -> 
+            let rndDataA = JaggedArray.shuffleColumnWiseInPlace dataA'
+            let rndDataU = JaggedArray.shuffleColumnWiseInPlace dataU'
+            calculate s0 rndDataA rndDataU
+            )
+        |> JaggedArray.transpose
+        |> Array.map (fun tt -> 
+            let (ri',si',di') = tt |> Array.fold (fun (ri,si,di) t -> ri+t.Ri,si+t.Si,di + t.Statistics) (0.,0.,0.)  
+            createSAM (ri' / float iterations) (si' / float iterations) (di' / float iterations)
+            )
+
+    /// 
+    let permutationBalancedSAM iterations s0 (dataA:float array array) (dataU:float array array) =
+        //let rnd = System.Random()
+        let dataA' = JaggedArray.copy dataA
+        let dataU' = JaggedArray.copy dataU
+    
+        [|0..iterations-1|]
+        |> Array.map (fun _ -> 
+            let rndDataA = JaggedArray.shuffleInPlace dataA'
+            let rndDataU = JaggedArray.shuffleInPlace dataU'
+            calculate s0 rndDataA rndDataU
+            )
+        |> JaggedArray.transpose
+        |> Array.map (fun tt -> 
+            let (ri',si',di') = tt |> Array.fold (fun (ri,si,di) t -> ri+t.Ri,si+t.Si,di + t.Statistics) (0.,0.,0.)  
+            createSAM (ri' / float iterations) (si' / float iterations) (di' / float iterations)
+            )
+
+    ///
+    let permutationCompleteSAM iterations s0 (dataA:float array array) (dataU:float array array) =
+        let rnd = System.Random()
+        let dataA' = JaggedArray.copy dataA
+        let dataU' = JaggedArray.copy dataU
+        let data = Array.zip dataA' dataU'
+
+
+        // Case specific FisherYates shuffling 
+        let shuffleInPlace' (random:System.Random) (arr : array<('a[]*'a[])>) = 
+            let rowCount =  arr.Length
+            let colCountFst,colCountSnd = 
+                if rowCount < 1 then 0,0 else Array.length ( fst(arr.[0]) ), Array.length ( snd(arr.[0]) )
+            let colCount = colCountFst + colCountSnd
+    
+            let getByIndex i (darr:'a[]*'a[]) =
+                if i > colCountFst-1 then (snd(darr)).[i-colCountFst] else (fst(darr)).[i]
+
+            let setByIndexInPlace i value (darr:'a[]*'a[]) =
+                if i > colCountFst-1 then (snd(darr)).[i-colCountFst] <- value else (fst(darr)).[i] <- value
+
+
+            for colI = colCount downto 1 do
+                for rowI = rowCount downto 1 do
+                    // Pick random element to swap.
+                    let i = random.Next(rowI) // 0 <= i <= rowI-1
+                    let j = random.Next(colI) // 0 <= j <= colI-1
+                    // Swap.
+                    let tmp = arr.[i] |> getByIndex j
+                    arr.[i] |> setByIndexInPlace j (arr.[rowI - 1] |> getByIndex (colI - 1))
+                    arr.[rowI - 1] |> setByIndexInPlace (colI - 1) tmp
+            arr 
+    
+        [|0..iterations-1|]
+        |> Array.map (fun _ -> 
+            let rndData = shuffleInPlace' rnd data        
+            let rndDataA,rndDataU = Array.unzip rndData
+            calculate s0 rndDataA rndDataU
+            )
+        |> JaggedArray.transpose
+        |> Array.map (fun tt -> 
+            let (ri',si',di') = tt |> Array.fold (fun (ri,si,di) t -> ri+t.Ri,si+t.Si,di + t.Statistics) (0.,0.,0.)  
+            createSAM (ri' / float iterations) (si' / float iterations) (di' / float iterations)
+            )
+
+
     /// Module with different estimation methods computing s0 
     module S0 =
-        open System.Runtime.CompilerServices
-        open PvalueAdjust.Qvalue
-        open System.Reflection
-        
-        // <summary>
+                
+
         /// Computes s0 using the nearest rank method
-        // <summary>
-        //
-        /// <param name="percentile" >float</param>
-        /// <param name="sis" >array of float</param>
-        /// <param name="tt" >array of float</param>
-        /// <param name="len" >int</param>
-        /// <returns> s0 via nearest rank calulation </returns> 
-        let nearestRank percentile (sis:array<float>) =
+        let nearestRank percentile (tt:array<SAM>) =
             if percentile < 0. || percentile > 1.0 then failwith "Percentile must be between 0.0 and 1.0"
-            let sissorted= 
-                sis
-                |> Array.sort
-            let index = System.Math.Round( percentile * float sis.Length, 0)
+            let sortedTT= 
+                tt
+                |> Array.sortBy (fun t -> t.Si)
+            let index = System.Math.Round( percentile * float tt.Length, 0)
             
-            sissorted.[(int index)]
-
-
-        // <summary>
-        /// Creates s0 by calulating the mean of the si values
-        // <summary>
-        //
-        /// <param name="sis" >array of float</param>
-        /// <param name="tt" >array of float</param>
-        /// <param name="len" >int</param>
-        /// <returns> s0 via median calulation </returns> 
-        let meadian (sis:array<float>) = Array.median sis
-
+            sortedTT.[(int index)].Si
 
         
+        /// Creates s0 by calulating the mean of si (pooled standard error )
+        let median (tt:array<SAM>) = 
+            tt
+            |> Array.map (fun t -> t.Si)
+            |> Array.median
 
-        // <summary>
+
         /// Estimates s0. Chosen to minimize the coefficient of variation of the test statistics 
-        // <summary>
-        //
-        /// <param name="sd" >combined standard deviation</param>
-        /// <param name="tt" >test statistcs</param>
-        /// <returns> s0 via estimation </returns> 
-        let estimate (sd:array<float>) (tt:array<float>) =
+        let estimate (tt:array<SAM>) = //(sd:array<float>) (tt:array<float>) =
             // Refactor to Care
             let partition predicate source =
                 let map =
@@ -78,17 +167,17 @@ module SAM =
                 loop source
 
 
-            let sd' = Array.copy sd
+            let si    = tt |> Array.map (fun t -> t.Si)
+            let stats = tt |> Array.map (fun t -> t.Statistics)
             let s0percentile= [|0. .. 0.05 .. 1.|] 
-            let br = [|0. .. 0.01 .. 1.|] |> Array.map (fun p -> Quantile.InPlace.modeInPLace p sd')            
+            let br = [|0. .. 0.01 .. 1.|] |> Array.map (fun p -> Quantile.InPlace.modeInPLace p si)            
 
             let cvSd =
-                let tmp = Seq.zip sd tt
+                let tmp = Seq.zip si stats
                 s0percentile
                 |> Array.map (fun k -> 
-                    let w  = Quantile.InPlace.modeInPLace k sd'
+                    let w  = Quantile.InPlace.modeInPLace k si
                     // Calc corrected test statistic
-                    //let tt' = Array.map2 (fun t s -> s,t * s / (s + w)) tt sd
                     let mads = 
                         bind (fun a b -> fst b < a) br tmp
                         |> Seq.map (fun v ->
@@ -101,7 +190,13 @@ module SAM =
                     )
                                 
             let pMinCvSd = cvSd |> Array.minBy snd |> fst // kleinste oder größte quantile mit kleinster cv
-            Quantile.InPlace.modeInPLace pMinCvSd sd'
+            Quantile.InPlace.modeInPLace pMinCvSd si
 
 
-    let a = 42
+        /// Estimates s0 from given data set. Chosen to minimize the coefficient of variation of the test statistics 
+        let estimateFrom (dataA:float array array) (dataU:float array array) =
+            let tt = calculate 0.0 dataA dataU
+            estimate tt
+
+
+    
