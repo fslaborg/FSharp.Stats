03395839df1a5654670e02d42824337d3e6119f9
HLWeil <hl.weil@t-online.de>
2018-03-09 18:32:53 +0100

    add tukey outlier detection

--- src/FSharp.Stats/Testing/Outliers.fs
+++ src/FSharp.Stats/Testing/Outliers.fs
@@ -3,8 +3,19 @@
 
 module Outliers =
     open FSharp.Stats
+    open FSharp.Stats
 
     type OutlierBorders =
         {Upper : float;
          Lower : float}
 
+    let createOutlierBorder upper lower = 
+        {Upper = upper; Lower = lower}
+
+    let tukey (c:float) (d:float []) =
+        let thirdQ = Quantile.compute 0.75 d 
+        let firstQ = Quantile.compute 0.25 d
+        let iqr = System.Math.Abs (thirdQ - firstQ)
+        createOutlierBorder (thirdQ + c * iqr) (firstQ - c * iqr)
+        
+        
