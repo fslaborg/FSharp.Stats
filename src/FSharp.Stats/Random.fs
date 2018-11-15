cda7732088a6c001c47558a1fc293715066d900a
ZimmerD <davidzimmer91@gmail.com>
2018-11-15 13:50:05 +0100

    add type RandBasic (not threadsafe) to Random.fs

--- src/FSharp.Stats/Random.fs
+++ src/FSharp.Stats/Random.fs
@@ -13,7 +13,7 @@ module Random =
     end
 
     ///// A standard implementation of a uniform random source using System.Random()
-    type RandBasic =
+    type RandThreadSafe =
         val mutable rnd : ThreadLocal<Random>
         /// Constructs the default random number generator with seed 17.
         new() = { rnd = new ThreadLocal<Random>(fun () -> new Random(17)) }
@@ -28,8 +28,23 @@ module Random =
             member x.NextFloat() =x.rnd.Value.NextDouble()
         end
 
+    type RandBasic =
+        val mutable rnd : Random
+        /// Constructs the default random number generator with seed 17.
+        new() = { rnd = new Random(17) }
+        /// If n is negative, the random number generator seed is based on system time, if it is zero or positive it will
+        /// use n as the seed.
+        new(n) as this = { rnd = new Random() }
+                         then
+                            if n >= 0 then this.rnd <- new Random(n)
+        interface IRandom with
+            member x.NextInt() = x.rnd.Next()
+            member x.NextInt maxValue = x.rnd.Next(maxValue)
+            member x.NextFloat() =x.rnd.NextDouble()
+        end
+
     /// The uniform random source used for sampling functions.
-    let mutable rndgen = new RandBasic() :> IRandom
+    let mutable rndgen = new RandThreadSafe() :> IRandom
     /// Sets the random number generator used for sampling.
     let SetSampleGenerator rg = rndgen <- rg
     /// Returns the random number generator used for sampling.
