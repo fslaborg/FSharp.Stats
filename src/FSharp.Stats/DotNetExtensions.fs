bb35457a082100745f4735699f4066629cac55b4
Adrian Zimmer <z.adrian1995@gmail.com>
2018-11-30 14:58:10 +0100

    Refactored

--- src/FSharp.Stats/DotNetExtensions.fs
+++ src/FSharp.Stats/DotNetExtensions.fs
@@ -1,15 +1,16 @@
-﻿module DotNetExtensions
+﻿namespace FSharp.Stats
 
-open System.Collections.Generic
+module DotNetExtensions =
+    open System.Collections.Generic
 
-[<Extension>]
-type List<'T> with
-    member m.Front
-        with get() = m.[0]
-        and set(value : 'T) = m.[0] <- value
-    member m.PopFront =
-        let value = m.[0]
-        m.RemoveAt(0)
-        value
-    member m.IsEmpty =
-        m.Count <= 0
+    [<Extension>]
+    type List<'T> with
+        member m.Front
+            with get() = m.[0]
+            and set(value : 'T) = m.[0] <- value
+        member m.PopFront =
+            let value = m.[0]
+            m.RemoveAt(0)
+            value
+        member m.IsEmpty =
+            m.Count <= 0
