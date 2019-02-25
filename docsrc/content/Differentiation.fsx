23242e16cf4f8ff6df3a8a405afae25270e28855
Kevin F <Freymaurer@gmx.de>
2019-02-25 15:31:38 +0100

    add image to docsrc and some addition minor changes to make it more understandable

--- docsrc/content/Differentiation.fsx
+++ docsrc/content/Differentiation.fsx
@@ -6,8 +6,14 @@
 Differentiation
 ========================
 Numerical differentiation is used to estimate the derivative of a mathematical function using values of the function and perhaps other knowledge about the function.
-To achieve this, it calculates the difference of f(x) at x and x+h and correlates it to h. This will give better approximations the smaller h is. 
+To achieve this, it calculates the difference of f(x) at x and f(x) at x+h and correlates it to h. This will give better approximations the smaller h is. 
 The function uses two different mathematical approaches to decrease the error, one for h > 1. and one for h < 1.
+
+</br>
+
+![Data model]https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Derivative.svg/800px-Derivative.svg.png
+
+</br>
 *)
 #r "FSharp.Stats.dll"
 open FSharp.Stats.Integration.Differentiation.TwoPointDifferentiation
