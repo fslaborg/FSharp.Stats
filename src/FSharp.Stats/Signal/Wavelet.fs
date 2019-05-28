60c6b16710d79bd0e87da8b6e0d1f6d8c33ecde4
bvenn <venn@rhrk.uni-kl.de>
2019-05-28 09:06:39 +0200

    add Ricker wavelet

--- src/FSharp.Stats/Signal/Wavelet.fs
+++ src/FSharp.Stats/Signal/Wavelet.fs
@@ -1,9 +1,37 @@
 namespace FSharp.Stats.Signal
 
 open FSharp.Stats
-
+open System
 
 
 module Wavelet =
-    
-    let mexicanHat x = raise (System.NotImplementedException())
+
+    ///Ricker, or Mexican hat wavelet
+    type Ricker = {
+        //the scale of the wavelet
+        Scale       : float
+        //half of the width of the wavelet
+        PaddingArea : float
+        //x_value of minimum y_value
+        MinimumPosX : float
+        //function that takes a x_value and gives the corresponding y_value
+        RickerFun   : (float -> float)
+        }
+
+    //creation function for Ricker
+    let createRicker scale =  
+        let rickerFun x = 
+            let xx = pown x 2
+            let ss = pown scale 2
+            let fakA = 2. / (sqrt(3. * scale) * 1.331335364)//(Math.PI**0.25)) pre calculated for efficiency
+            let fakB = 1. - (xx/ss)
+            let fakC = exp (-xx / (2. * ss))
+            fakA * fakB * fakC
+        let padArea =   (7. * scale) |> ceil
+        let minimumPosX = 1.73205 * scale
+        {
+        Scale       = scale
+        PaddingArea = padArea
+        MinimumPosX = minimumPosX
+        RickerFun   = rickerFun
+        }
