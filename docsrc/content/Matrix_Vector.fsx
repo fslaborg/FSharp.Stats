869035ea53ff943d0b598a54c420b1a1797605be
Kevin Frey <39732517+Freymaurer@users.noreply.github.com>
2019-09-03 15:37:39 +0200

    update Vector and Matrix functions to new naming conventions
    * apply changes requested by @bvenn for issue #46
    
    * update Vector.fs and Matrix_Vector.fsx
    - changes in other files are follow up changes, due to name changes in Vector.fs
    - This should partly fix issue #43
    - change input parameter position some functions as requested by @bvenn
    - Adjust parameter names to FSharp.Core naming
    - clean up docs
    - clean up function names (remove underscored)
    
    * small correcture for docs
    
    * remove Array.set leftover in doc
    
    * update docs for requested changed by @bvenn

--- docsrc/content/Matrix_Vector.fsx
+++ docsrc/content/Matrix_Vector.fsx
@@ -1,6 +1,7 @@
 (*** hide ***)
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
+//#r "D:/Source/FSharp.Stats/bin/FSharp.Stats.dll"
 #I "../../bin/FSharp.Stats/netstandard2.0"
 #r "../../packages/formatting/FSharp.Plotly/lib/netstandard2.0/Fsharp.Plotly.dll"
 #r "netstandard"
@@ -13,25 +14,47 @@ open FSharp.Plotly
 <a name="Vector"></a>
 
 ##Vector
+*)
+#r "FSharp.Stats.dll"
+open FSharp.Stats
 
+let v = 
+    vector [|2.0; 20.0; 1.|]
+
+(*** include-value:v ***)
+
+let rv = 
+    rowvec [|2.0; 20.0; 1.|]
+
+(*** include-value:rv ***)
+
+(***hide***)
+let exmp13 = v + 1.
+let exmp14 = rv + 1.
+
+(**Addition to every element of vector.*)
+v + 1.
+
+(*** include-value:exmp13 ***)
+
+(**Addition to every element of row vector.*)
+rv + 1.
+
+(*** include-value:exmp14 ***)
+
+
+(**
 <a name="Matrix"></a>
 
 ##Matrix
 
-
-
+###Creating Matrices with FSharp.Stats. 
+Matrices will also be printed with the FSharp.Stats.FSIPrinters.matrix function to get a nice console output.
 *)
-//#r "D:/Source/FSharp.Stats/bin/FSharp.Stats.dll"
 #r "FSharp.Stats.dll"
-//open FSharp
-open Microsoft.FSharp.Math
 open FSharp.Stats
-open FSharp.Stats.Algebra
-
-
 
 // http://fdatamining.blogspot.de/2010/03/matrix-and-linear-algebra-in-f-part-i-f.html
-// 1.9.7.8. 
 // http://fdatamining.blogspot.de/search/label/linear%20algebra
 
 let A = 
@@ -39,60 +62,115 @@ let A =
              [ 1.0; 3.0; 1.0 ]
              [ 2.0; 9.0; 1.0 ] ]
 
+(***hide***)
+let APrint =
+    FSharp.Stats.FSIPrinters.matrix A
+
+(*** include-value:APrint ***)
+
 let B = 
     matrix [ [ 10.0; 70.0; 20.0 ]
              [ 10.0; 30.0; 10.0 ]
              [ 20.0; 90.0; 10.0 ] ]
 
+(***hide***)
+let BPrint =
+    FSharp.Stats.FSIPrinters.matrix B
 
-let v = 
-    vector [|2.0; 20.0; 1.|]
-
-
-let rv = 
-    rowvec [|2.0; 20.0; 1.|]
+(*** include-value:BPrint ***)
 
 
+(**
+###Calculation Examples for Matrices
+*)
+
+(***hide***)
+let exmp1 =
+    A
+    |> Matrix.Generic.enumerateRowWise (Seq.sum)
+
+(**Get sum of each row.*)
 A
 |> Matrix.Generic.enumerateRowWise (Seq.sum)
 
+(*** include-value:exmp1 ***)
+
+(***hide***)
+let exmp2 =
+    A
+    |> Matrix.mapiCols (fun i r -> r)
+
+(**Create seq of vectors of matrix columns.*)
 A
 |> Matrix.mapiCols (fun i r -> r)
 
-//let f x = x**2.
+(*** include-value:exmp2 ***)
 
-//"2.;3.;".Split(';') 
-//|> Array.filter (fun x -> x <> "") 
-//|> Array.map float 
-//|> Array.map f
+(***hide***)
+let exmp3 = A + B |> FSharp.Stats.FSIPrinters.matrix
+let exmp4 = A - B|> FSharp.Stats.FSIPrinters.matrix
+let exmp5 = A * B|> FSharp.Stats.FSIPrinters.matrix
+let exmp6 = A.*B|> FSharp.Stats.FSIPrinters.matrix
+let exmp7 = A * 2.0|> FSharp.Stats.FSIPrinters.matrix
+let exmp9 = -A|> FSharp.Stats.FSIPrinters.matrix
+let exmp10 = A * v 
+let exmp11 = Matrix.dot A B 
+let exmp12 = A + 1.|> FSharp.Stats.FSIPrinters.matrix
 
 
-
+(**Sum of two matrices.*)
 A + B
-A-B
-A*B // matrix product
-A.*B  // element-wise product
-A * 2.0 // scalar product
-2.0 * A // this is also ok
--A // negation of a matrix
 
-//  matrix-vector product
+(*** include-value:exmp3 ***)
+
+(**Difference between two matrices.*)
+A - B
+
+(*** include-value:exmp4 ***)
+
+(**Product of two matrices.*)
+A * B 
+
+(*** include-value:exmp5 ***)
+
+(**Element-wise product of two matrices*)
+A .* B 
+
+(*** include-value:exmp6 ***)
+
+(**Scalar product of a matrix.*)
+A * 2.0 
+2.0 * A // also ok, gives same result
+
+(*** include-value:exmp7 ***)
+
+(**Negation of a matrix*)
+-A 
+
+(*** include-value:exmp9 ***)
+
+(**Product of a matrix-vector.*)
 A * v
 
+(*** include-value:exmp10 ***)
+
+(**Dot product of two matrices.*)
 Matrix.dot A B
 
-A + 1.
-v + 1.
-rv + 1.
+(*** include-value:exmp11 ***)
 
+(**Addition to every element of matrix.*)
+A + 1.
+
+(*** include-value:exmp12 ***)
+
+
+(***hide***)
 //let () =
 //    let assemblyProbeDirectory =  "D:/Source/FSharp.Stats/lib"
 //    System.IO.Directory.SetCurrentDirectory(assemblyProbeDirectory)
 
-
-
-
-
+(***hide***)
 //// Linear regression
 //let cost theta X (y:vector) =
 //    let tmp = Matrix.dot X theta 
@@ -101,11 +179,12 @@ rv + 1.
 //        let error = v - tmp
 //        sum + (error * error) ) 0. 
 //    |> (/) (float y.Length * 2.0)
- 
 
+(***hide***)
 //let inline (.-) a b =
-//    a |> Matrix.map (fun x -> x - b)    
+//    a |> Matrix.map (fun x -> x - b)
 
+(***hide***)
 //let estimateCoefficientsBySGD (X:matrix) y theta alpha = // rate nEpoch (data:array<array<'a>>)
 //    //let delta = Vector.create (X.NumCols) 1.
 //    let cost' = cost theta X y
@@ -119,33 +198,31 @@ rv + 1.
 //            let delta' = 
 //                Matrix.dot X.Transpose error 
 //                |> (/) (float y.Length)
- 
 //            let trial_theta = theta - alpha * delta
 //            let trial_cost = cost trial_theta X y
 //            loop delta'
-
 //    let rec wloop (theta:vector) (ttheta:vector) (tcost:vector) (pcost:vector) =
 //        let ttheta' = (theta + ttheta) / 2.
-//        let tcost'  = cost ttheta X y
-       
+//        let tcost'  = cost ttheta X y      
 //    //while (np.max(np.abs(delta)) > 1e-6) do
 //    //    1.
 
 
 
-
+(***hide***)
 //// F# Numerics Interface 
 //// http://fdatamining.blogspot.de/2010/03/f-inumerics-interface-and-matrix-class.html
 
-
+(***hide***)
 //// Service Provider model pattern
 //// 
 //// http://blog.ploeh.dk/2011/04/27/Providerisnotapattern/
 //// http://blog.ploeh.dk/2011/04/27/Providerisnotapattern/
 
-
+(***hide***)
 //open System.Runtime.InteropServices
 
+(***hide***)
 //// [<DllImport("C:/Program Files (x86)/Sho 2.1/bin/bin64/mkl.dll",EntryPoint="dgemm_")>]
 //[<DllImport("D:\\libopenblas.dll",EntryPoint="dgemm_")>]
 //extern void dgemm_
@@ -157,21 +234,18 @@ rv + 1.
 
 
 
-
+(***hide***)
 //#nowarn "51" 
-
 //open Microsoft.FSharp.NativeInterop
 
-
+(***hide***)
 //let matmul_blas (a:float[,]) (b:float[,]) = 
 //    // Get dimensions of the input matrices
 //    let m = Array2D.length1 a
 //    let k = Array2D.length2 a
 //    let n = Array2D.length2 b
- 
 //    // Allocate array for the result
 //    let c = Array2D.create n m 0.0
-
 //    // Declare arguments for the call
 //    let mutable arg_transa = 't'
 //    let mutable arg_transb = 't'
@@ -184,23 +258,22 @@ rv + 1.
 //    let mutable arg_beta = 1.0
 //    let mutable arg_ldm = m
 
+(***hide***)
 //    // Temporarily pin the arrays
 //    use arg_a = PinnedArray2.of_array2D(a)
 //    use arg_b = PinnedArray2.of_array2D(b)
 //    use arg_c = PinnedArray2.of_array2D(c)
-
 //    // Invoke the native routine
 //    dgemm_( &&arg_transa, &&arg_transb,
 //            &&arg_m, &&arg_n, &&arg_k,
 //            &&arg_alpha, arg_a.Ptr, &&arg_ldk,
 //            arg_b.Ptr, &&arg_ldn, &&arg_beta,
 //            arg_c.Ptr, &&arg_ldm )
-
 //    // Transpose the result to get m*n matrix 
 //    Array2D.init m n (fun i j -> c.[j,i])
 
 
 
-
+(***hide***)
 //matmul_blas (A.ToArray2D()) (B.ToArray2D())
 
