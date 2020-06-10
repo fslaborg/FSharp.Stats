b2017c0015aca533551fdfc9528db2bf20687f75
bvenn <venn@rhrk.uni-kl.de>
2020-06-10 14:08:57 +0200

    add confidence interval calculation #74

---
+++ src/FSharp.Stats/ConfidenceInterval.fs
@@ -0,0 +1,19 @@
+﻿namespace FSharp.Stats
+
+module ConfidenceInterval =
+
+    open FSharp.Stats.Distributions
+
+    // The deviation from the sample mean is calculated by using a t distribution. Common ciLevel = 0.95
+    let ciDeviation ciLevel (sample : seq<float>)=
+        let n = float (Seq.length sample)
+        let stDev = Seq.stDev sample
+        let t = Continuous.getCriticalTValue (float n - 1.) (1. - ciLevel) Continuous.TwoTailed
+        let delta = t * stDev / sqrt (float n)
+        delta
+
+    // The confidence interval is calculated by using a t distribution. Common ciLevel = 0.95
+    let ci ciLevel (sample : seq<float>)=
+        let mean  = Seq.mean  sample
+        let delta = ciDeviation ciLevel sample
+        Intervals.create (mean - delta) (mean + delta)
