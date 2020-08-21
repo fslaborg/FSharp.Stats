02962581401c6139ddce07042596cd4b9e86b61e
Aeru28 <63650208+Aeru28@users.noreply.github.com>
2020-08-21 11:07:30 +0200

    add FTest module

---
+++ src/FSharp.Stats/Testing/FTest.fs
@@ -0,0 +1,24 @@
+﻿namespace FSharp.Stats.Testing
+
+module FTest = 
+
+    open FSharp.Stats
+
+    /// F-Test to compare two variances from data
+    let testVariancesFromData (data1:Vector<float>) (data2:Vector<float>) = 
+        if data1.Length = 0 || data2.Length = 0 then failwithf "Data cannot be empty."
+        let var1 = Seq.var data1
+        let var2 = Seq.var data2
+        let df1 = float data1.Length - 1.
+        let df2 = float data2.Length - 1.
+        if var1 > var2 then
+            let statistic = var1 / var2
+            Testing.TestStatistics.createFTest statistic df1 df2
+        else
+            let statistic = var2 / var1
+            Testing.TestStatistics.createFTest statistic df2 df1
+
+    /// F-Test to compare two variances from given parameters.
+    let testVariancesFromVarAndDof (var1:float)(var2:float)(df1:float)(df2:float) =
+        let statistic = var1 / var2 
+        Testing.TestStatistics.createFTest statistic df1 df2
