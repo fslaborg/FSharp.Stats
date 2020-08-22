ba5cb67801590a70e11b0f030544b793b321ee80
ZieglerSe <63650208+Aeru28@users.noreply.github.com>
2020-08-22 19:54:33 +0200

    Update F-Test

--- src/FSharp.Stats/Testing/FTest.fs
+++ src/FSharp.Stats/Testing/FTest.fs
@@ -4,7 +4,7 @@ module FTest =
 
     open FSharp.Stats
 
-    /// F-Test to compare two variances from data
+    /// F-Test to compare two variances from data.
     let testVariancesFromData (data1:Vector<float>) (data2:Vector<float>) = 
         if data1.Length = 0 || data2.Length = 0 then failwithf "Data cannot be empty."
         let var1 = Seq.var data1
@@ -19,6 +19,11 @@ module FTest =
             Testing.TestStatistics.createFTest statistic df2 df1
 
     /// F-Test to compare two variances from given parameters.
-    let testVariancesFromVarAndDof (var1:float)(var2:float)(df1:float)(df2:float) =
-        let statistic = var1 / var2 
-        Testing.TestStatistics.createFTest statistic df1 df2
+    let testVariancesFromVarAndDof (var1:float,df1:float) (var2:float,df2:float)  =
+        if df1 = 0. || df2 = 0. then failwithf "Degrees of freedom cannot be zero."
+        if var1 > var2 then 
+            let statistic = var1/var2 
+            FSharp.Stats.Testing.TestStatistics.createFTest statistic df1 df2 
+        else
+            let statistic = var2 / var1
+            FSharp.Stats.Testing.TestStatistics.createFTest statistic df2 df1
