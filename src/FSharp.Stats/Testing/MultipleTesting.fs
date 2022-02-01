f08da6a1355c43b40a55de3736493b968459491a
omaus <omaus@bio.uni-kl.de>
2022-02-01 16:54:45 +0100

    Apply code review changes :ok_hand:

--- src/FSharp.Stats/Testing/MultipleTesting.fs
+++ src/FSharp.Stats/Testing/MultipleTesting.fs
@@ -64,12 +64,15 @@ module MultipleTesting =
 
 
     // adapted from: https://en.wikipedia.org/wiki/%C5%A0id%C3%A1k_correction   &   https://personal.utdallas.edu/~herve/Abdi-Bonferroni2007-pretty.pdf
-    // Old: /// Computes the Dunn-Šidák correction onto a collection of p-values with a given alpha. Returns a list of booleans which state if the p-value should be rejected or not, and the new critical value for this alpha.
-    /// Computes the Dunn-Šidák correction onto a collection of p-values with a given alpha. Returns a critical value. p-values above the critical value should be rejected.
+    // checked according to: https://cran.r-project.org/web/packages/aggregation/index.html
+    // original derivation: https://www.tandfonline.com/doi/abs/10.1080/01621459.1967.10482935
+    /// <summary>Computes the Dunn-Sidak correction onto a collection of p-values with a given alpha.</summary>
+    /// <returns>Returns a critical value.</returns>
+    /// <remarks>p-values above the critical value should be rejected. Use this correction to reduce family-wise error rate when performing multiple tests on a dataset. p-values above 1, below 0, inf, and nan are excluded.</remarks>
     let dunnSidak alpha (pValues : seq<float>) = 
-        let m = float (Seq.length pValues)
+        let checkedValues = Seq.filter (fun v -> v >= 0. && v <= 1.) pValues
+        let m = float (Seq.length checkedValues)
         let criticalValue = 1. - (1. - alpha) ** (1. / m)
-        // List.map (fun p -> if p > criticalValue then true, p else false, p) (List.ofSeq pValues),
         criticalValue
 
 
