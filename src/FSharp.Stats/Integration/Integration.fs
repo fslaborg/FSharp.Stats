dc3ee45d23c6b4729de30223f244e9910e4394a9
Kevin Schneider <schneider.kev@outlook.de>
2022-03-31 19:54:07 +0200

    Rework integration module, add observation integration and tests

--- src/FSharp.Stats/Integration/Integration.fs
+++ src/FSharp.Stats/Integration/Integration.fs
@@ -1,5 +1,111 @@
 namespace FSharp.Stats.Integration
 
+open FSharp.Stats
+open FSharpAux
+
+/// Approximation methods for definitive integral estimation
+type NumericalIntegrationMethod =
+    | LeftEndpoint
+    | RightEndpoint
+    | MidPoint
+    | Trapezoidal
+    | Simpson
+
+    static member integrateFunction = function 
+        | LeftEndpoint -> fun (f: (float -> float)) leftBoundaries partitionSize -> leftBoundaries |> Seq.sumBy (fun boundary -> f(boundary) * partitionSize)
+        | RightEndpoint -> fun (f: (float -> float)) leftBoundaries partitionSize -> leftBoundaries |> Seq.sumBy (fun boundary -> f(boundary + partitionSize) * partitionSize)
+        | MidPoint -> fun (f: (float -> float)) leftBoundaries partitionSize -> leftBoundaries |> Seq.sumBy (fun boundary -> f(boundary + (partitionSize / 2.)) * partitionSize)
+        | Trapezoidal -> fun (f: (float -> float)) leftBoundaries partitionSize -> leftBoundaries |> Seq.sumBy (fun boundary -> ((f(boundary) + f(boundary + partitionSize)) * partitionSize) / 2.)
+        | Simpson -> fun (f: (float -> float)) leftBoundaries partitionSize -> leftBoundaries |> Seq.sumBy (fun boundary -> ((f(boundary) + 4.0 * f(boundary + partitionSize / 2.0) + f(boundary + partitionSize)) * partitionSize) / 6.0)
+
+    static member integrateObservations = function
+        | LeftEndpoint -> fun (observations: (float*float) []) ->
+            let xVals, yVals = observations |> Array.unzip
+            xVals
+            |> Array.foldi (fun i auc x ->
+                if i = xVals.Length - 1 then 
+                    auc 
+                else 
+                    let rectWidth = xVals[i+1] - x
+                    (auc + (rectWidth*yVals[i]))
+            ) 0.
+        | RightEndpoint -> fun (observations: (float*float) []) ->
+            let xVals, yVals = observations |> Array.unzip
+            xVals
+            |> Array.foldi (fun i auc x ->
+                if i = 0 then 
+                    auc 
+                else 
+                    let rectWidth = x - xVals[i-1]
+                    (auc + (rectWidth*yVals[i]))
+            ) 0.
+        | MidPoint -> fun (observations: (float*float) []) ->
+            let xVals, yVals = observations |> Array.unzip
+            xVals
+            |> Array.foldi (fun i auc x ->
+                if i = xVals.Length - 1 then 
+                    auc 
+                else 
+                    let midValue = (yVals[i] + yVals[i+1]) / 2.
+                    let rectWidth = xVals[i+1] - x
+                    (auc + (rectWidth*midValue))
+            ) 0.
+        | Trapezoidal -> fun (observations: (float*float) []) ->
+            let xVals, yVals = observations |> Array.unzip
+            xVals
+            |> Array.foldi (fun i auc x ->
+                if i = xVals.Length - 1 then 
+                    auc 
+                else 
+                    let trapezoid = (xVals[i+1] - x) * ((yVals[i] + yVals[i+1]) / 2.)
+                    (auc + trapezoid)
+            ) 0.
+        | Simpson -> fun (observations: (float*float) []) ->
+            let xVals, yVals = observations |> Array.unzip
+            xVals
+            |> Array.foldi (fun i auc x ->
+                if i = xVals.Length - 1 then 
+                    auc 
+                else 
+                    let midValue = (yVals[i] + yVals[i+1]) / 2.
+                    let parabola = ((xVals[i+1] - x) / 6.) * (yVals[i] + (4. * midValue) + yVals[i+1])
+                    (auc + parabola)
+            ) 0.
+
+/// Definite integral approximation
+type NumericalIntegration() = 
+    
+    /// Returns a function that performs numerical integration of the input with the given `method` for partitions of equal size `partitions` in an inclusive closed interval [`intervalStart`, `intervalEnd`]
+    static member integrateFunction(
+        /// the numerical integration approximation method to use
+        method: NumericalIntegrationMethod,
+        /// leftmost x value, boundary of the integration interval
+        intervalStart: float,
+        /// rightmost x value, boundary of the integration interval
+        intervalEnd: float,
+        /// the amount of partitions in the integration interval
+        partitions: int
+        ///// wether to run in parallel
+        //?Parallel: bool,
+        ///// how many threads to use when running in parallel
+        //?NThreads: bool
+    ) = 
+        fun (f: float -> float) ->
+            //size of the individual partitions
+            let partitionSize = (intervalEnd - intervalStart) / (float partitions)
+            //all left boundaries to use for integration methods
+            let leftBoundaries = [0 .. 1 .. partitions - 1] |> Seq.map (fun i -> intervalStart + (float i) * partitionSize)
+
+            NumericalIntegrationMethod.integrateFunction method f leftBoundaries partitionSize
+    
+    /// returns a function that approximates the AUC of the observations ((x,y) data) with the given numerical integration method
+    static member integrateObservations(
+        /// the numerical integration approximation method to use
+        method: NumericalIntegrationMethod
+    ) = 
+        fun (data: (float*float) []) -> NumericalIntegrationMethod.integrateObservations method data
+        
+
 module DefiniteIntegral =
 
     type IntegrationMethod = (float->float) -> float -> float -> float
---
+++ tests/FSharp.Stats.Tests/Integration.fs
@@ -0,0 +1,197 @@
+﻿module IntegrationTests
+open Expecto
+open System
+open FSharp.Stats
+
+open System.IO
+
+open FSharp.Stats.Integration
+
+[<Tests>]
+let numericalIntegrationTests =
+
+    /// f(x) = x^3
+    let f1 x = x * x * x
+    /// f(x) = 1/x
+    let f2 x = 1. / x
+
+    let observations1 = [|0. .. 0.0001 ..  1.|] |> Array.map (fun x -> x, f1 x)
+    let observations2 = [|1. .. 0.001 .. 100.|] |> Array.map (fun x -> x, f2 x)
+
+    //
+    let fNaN x = nan 
+    let fPosInf x = infinity 
+    let fNegInf x = -infinity
+
+    let observationsNaN = [|0. .. 0.0001 ..  1.|] |> Array.map (fun x -> x, fNaN x)
+    let observationsPosInf = [|0. .. 0.0001 ..  1.|] |> Array.map (fun x -> x, fPosInf x)
+    let observationsNegInf = [|0. .. 0.0001 ..  1.|] |> Array.map (fun x -> x, fNegInf x)
+
+    testList "NumericalIntegration" [ 
+        testList "function integration" [
+            testCase "LeftEndpoint x^3" (fun _ ->
+                let actual = f1 |> NumericalIntegration.integrateFunction(LeftEndpoint, 0., 1., 10000)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.low actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "RightEndpoint x^3" (fun _ ->
+                let actual = f1 |> NumericalIntegration.integrateFunction(RightEndpoint, 0., 1., 10000)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.low actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "MidPoint x^3" (fun _ ->
+                let actual = f1 |> NumericalIntegration.integrateFunction(MidPoint, 0., 1., 10000)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Trapezoidal x^3" (fun _ ->
+                let actual = f1 |> NumericalIntegration.integrateFunction(Trapezoidal, 0., 1., 10000)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Simpson x^3" (fun _ ->
+                let actual = f1 |> NumericalIntegration.integrateFunction(Simpson, 0., 1., 10000)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.veryHigh actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "LeftEndpoint 1/x" (fun _ ->
+                let actual = f2 |> NumericalIntegration.integrateFunction(LeftEndpoint, 1., 100., 100000)
+                //exact result is 0.25
+                let expected = log(100.)
+                Expect.floatClose Accuracy.low actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "RightEndpoint 1/x" (fun _ ->
+                let actual = f2 |> NumericalIntegration.integrateFunction(RightEndpoint, 1., 100., 100000)
+                //exact result is 0.25
+                let expected = log(100.)
+                Expect.floatClose Accuracy.low actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "MidPoint 1/x" (fun _ ->
+                let actual = f2 |> NumericalIntegration.integrateFunction(MidPoint, 1., 100., 100000)
+                //exact result is 0.25
+                let expected = log(100.)
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Trapezoidal 1/x" (fun _ ->
+                let actual = f2 |> NumericalIntegration.integrateFunction(Trapezoidal, 1., 100., 100000)
+                //exact result is 0.25
+                let expected = log(100.)
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Simpson 1/x" (fun _ ->
+                let actual = f2 |> NumericalIntegration.integrateFunction(Simpson, 1., 100., 100000)
+                //exact result is 0.25
+                let expected = log(100.)
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+        ]
+        testList "observation integration" [ 
+            testCase "LeftEndpoint x^3" (fun _ ->
+                let actual = observations1 |> NumericalIntegration.integrateObservations(LeftEndpoint)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.low actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "RightEndpoint x^3" (fun _ ->
+                let actual = observations1 |> NumericalIntegration.integrateObservations(RightEndpoint)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.low actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "MidPoint x^3" (fun _ ->
+                let actual = observations1 |> NumericalIntegration.integrateObservations(MidPoint)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Trapezoidal x^3" (fun _ ->
+                let actual = observations1 |> NumericalIntegration.integrateObservations(Trapezoidal)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Simpson x^3" (fun _ ->
+                let actual = observations1 |> NumericalIntegration.integrateObservations(Simpson)
+                //exact result is 0.25
+                let expected = 0.25
+                Expect.floatClose Accuracy.high actual expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "LeftEndpoint 1/x" (fun _ ->
+                let actual = observations2 |> NumericalIntegration.integrateObservations(LeftEndpoint)
+                //exact result is 0.25
+                let expected = round 5 (log 100.)
+                Expect.floatClose Accuracy.low (round 5 actual) expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "RightEndpoint 1/x" (fun _ ->
+                let actual = observations2 |> NumericalIntegration.integrateObservations(RightEndpoint)
+                //exact result is 0.25
+                let expected = round 5 (log 100.)
+                Expect.floatClose Accuracy.low (round 5 actual) expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "MidPoint 1/x" (fun _ ->
+                let actual = observations2 |> NumericalIntegration.integrateObservations(MidPoint)
+                //exact result is 0.25
+                let expected = round 5 (log 100.)
+                Expect.floatClose Accuracy.high (round 5 actual) expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Trapezoidal 1/x" (fun _ ->
+                let actual = observations2 |> NumericalIntegration.integrateObservations(Trapezoidal)
+                //exact result is 0.25
+                let expected = round 5 (log 100.)
+                Expect.floatClose Accuracy.high (round 5 actual) expected "LeftEndpoint did not return the correct result"
+            )
+            testCase "Simpson 1/x" (fun _ ->
+                let actual = observations2 |> NumericalIntegration.integrateObservations(Simpson)
+                //exact result is 0.25
+                let expected = round 5 (log 100.)
+                Expect.floatClose Accuracy.high (round 5 actual) expected "LeftEndpoint did not return the correct result"
+            )
+        ]
+        testList "integrating nan function returns nan" [
+            testCase "LeftEndpoint" (fun _ -> Expect.isTrue (nan.Equals(fNaN |> NumericalIntegration.integrateFunction(LeftEndpoint,0.,1.,1000))) "did not return nan")
+            testCase "RightEndpoint" (fun _ -> Expect.isTrue (nan.Equals(fNaN |> NumericalIntegration.integrateFunction(RightEndpoint,0.,1.,1000)))  "did not return nan")
+            testCase "MidPoint" (fun _ -> Expect.isTrue (nan.Equals(fNaN |> NumericalIntegration.integrateFunction(MidPoint,0.,1.,1000)))  "did not return nan")
+            testCase "Trapezoidal" (fun _ -> Expect.isTrue (nan.Equals(fNaN |> NumericalIntegration.integrateFunction(Trapezoidal,0.,1.,1000)))  "did not return nan")
+            testCase "Simpson" (fun _ -> Expect.isTrue (nan.Equals(fNaN |> NumericalIntegration.integrateFunction(Simpson,0.,1.,1000)))  "did not return nan")
+        ]
+        testList "integrating +infinity function returns +infinity" [
+            testCase "LeftEndpoint" (fun _ -> Expect.equal (fPosInf |> NumericalIntegration.integrateFunction(LeftEndpoint,0.,1.,1000)) infinity "did not return infinity")
+            testCase "RightEndpoint" (fun _ -> Expect.equal (fPosInf |> NumericalIntegration.integrateFunction(RightEndpoint,0.,1.,1000)) infinity "did not return infinity")
+            testCase "MidPoint" (fun _ -> Expect.equal (fPosInf |> NumericalIntegration.integrateFunction(MidPoint,0.,1.,1000)) infinity "did not return infinity")
+            testCase "Trapezoidal" (fun _ -> Expect.equal (fPosInf |> NumericalIntegration.integrateFunction(Trapezoidal,0.,1.,1000)) infinity "did not return infinity")
+            testCase "Simpson" (fun _ -> Expect.equal (fPosInf |> NumericalIntegration.integrateFunction(Simpson,0.,1.,1000)) infinity "did not return infinity")
+        ]
+        testList "integrating +infinity function returns -infinity" [
+            testCase "LeftEndpoint" (fun _ -> Expect.equal (fNegInf |> NumericalIntegration.integrateFunction(LeftEndpoint,0.,1.,1000)) -infinity "did not return -infinity")
+            testCase "RightEndpoint" (fun _ -> Expect.equal (fNegInf |> NumericalIntegration.integrateFunction(RightEndpoint,0.,1.,1000)) -infinity "did not return -infinity")
+            testCase "MidPoint" (fun _ -> Expect.equal (fNegInf |> NumericalIntegration.integrateFunction(MidPoint,0.,1.,1000)) -infinity "did not return -infinity")
+            testCase "Trapezoidal" (fun _ -> Expect.equal (fNegInf |> NumericalIntegration.integrateFunction(Trapezoidal,0.,1.,1000)) -infinity "did not return -infinity")
+            testCase "Simpson" (fun _ -> Expect.equal (fNegInf |> NumericalIntegration.integrateFunction(Simpson,0.,1.,1000)) -infinity "did not return -infinity")
+        ]
+        testList "integrating nan observations returns nan" [
+            testCase "LeftEndpoint" (fun _ -> Expect.isTrue (nan.Equals(observationsNaN |> NumericalIntegration.integrateObservations(LeftEndpoint))) "did not return nan")
+            testCase "RightEndpoint" (fun _ -> Expect.isTrue (nan.Equals(observationsNaN |> NumericalIntegration.integrateObservations(RightEndpoint))) "did not return nan")
+            testCase "MidPoint" (fun _ -> Expect.isTrue (nan.Equals(observationsNaN |> NumericalIntegration.integrateObservations(MidPoint))) "did not return nan")
+            testCase "Trapezoidal" (fun _ -> Expect.isTrue (nan.Equals(observationsNaN |> NumericalIntegration.integrateObservations(Trapezoidal))) "did not return nan")
+            testCase "Simpson" (fun _ -> Expect.isTrue (nan.Equals(observationsNaN |> NumericalIntegration.integrateObservations(Simpson))) "did not return nan")
+        ]
+        testList "integrating +infinity observations returns +infinity" [
+            testCase "LeftEndpoint" (fun _ -> Expect.equal (observationsPosInf |> NumericalIntegration.integrateObservations(LeftEndpoint)) infinity "did not return infinity")
+            testCase "RightEndpoint" (fun _ -> Expect.equal (observationsPosInf |> NumericalIntegration.integrateObservations(RightEndpoint)) infinity "did not return infinity")
+            testCase "MidPoint" (fun _ -> Expect.equal (observationsPosInf |> NumericalIntegration.integrateObservations(MidPoint)) infinity "did not return infinity")
+            testCase "Trapezoidal" (fun _ -> Expect.equal (observationsPosInf |> NumericalIntegration.integrateObservations(Trapezoidal)) infinity "did not return infinity")
+            testCase "Simpson" (fun _ -> Expect.equal (observationsPosInf |> NumericalIntegration.integrateObservations(Simpson)) infinity "did not return infinity")
+        ]
+        testList "integrating +infinity observations returns -infinity" [
+            testCase "LeftEndpoint" (fun _ -> Expect.equal (observationsNegInf |> NumericalIntegration.integrateObservations(LeftEndpoint)) -infinity "did not return -infinity")
+            testCase "RightEndpoint" (fun _ -> Expect.equal (observationsNegInf |> NumericalIntegration.integrateObservations(RightEndpoint)) -infinity "did not return -infinity")
+            testCase "MidPoint" (fun _ -> Expect.equal (observationsNegInf |> NumericalIntegration.integrateObservations(MidPoint)) -infinity "did not return -infinity")
+            testCase "Trapezoidal" (fun _ -> Expect.equal (observationsNegInf |> NumericalIntegration.integrateObservations(Trapezoidal)) -infinity "did not return -infinity")
+            testCase "Simpson" (fun _ -> Expect.equal (observationsNegInf |> NumericalIntegration.integrateObservations(Simpson)) -infinity "did not return -infinity")
+        ]
+    ]
