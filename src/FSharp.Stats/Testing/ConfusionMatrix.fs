bbe7151efb16c2aa312dd92a066d5330b6b75cf5
Kevin Schneider <schneider.kev@outlook.de>
2022-03-31 21:43:54 +0200

    add multi label confusion matrix

--- src/FSharp.Stats/Testing/ConfusionMatrix.fs
+++ src/FSharp.Stats/Testing/ConfusionMatrix.fs
@@ -48,3 +48,46 @@ type BinaryConfusionMatrix = {
     ) = BinaryConfusionMatrix.ofPredictions(true,actual,predictions)
 
 open System
+open FSharpAux
+
+type MultiLabelConfusionMatrix = {
+    Labels: string []
+    Confusion: Matrix<int>
+} with
+    static member create(labels: string [], confusion:Matrix<int>) =
+        if (labels.Length <> confusion.NumCols) || (labels.Length <> confusion.NumRows) then failwith "The confusion matrix must be square and the labels must contain as many values as the confusion dimensions."
+        {
+            Labels = labels
+            Confusion = confusion
+        }
+
+    static member ofPredictions(
+        labels: #IConvertible [],
+        actual: #IConvertible [],
+        predictions: #IConvertible []
+    ) = 
+        let zipped = Array.zip actual predictions
+
+        let confusion =
+            Matrix.Generic.init labels.Length labels.Length (fun m n ->
+                let labelM = labels[m]
+                let labelN = labels[n]
+                zipped
+                |> Array.filter (fun (truth,pred) ->
+                    truth = labelM && pred = labelN
+                )
+                |> Array.length
+            )
+
+        MultiLabelConfusionMatrix.create(labels |> Array.map string, confusion)
+
+    static member oneVsRest (mlcm: MultiLabelConfusionMatrix) =
+
+        mlcm.Labels
+        |> Array.mapi (fun labelIndex label ->
+            let tp = mlcm.Confusion[labelIndex,labelIndex]
+            let tn = mlcm.Confusion |> Matrix.Generic.foldi (fun rI cI acc elem -> if rI = labelIndex || cI = labelIndex then acc else acc + elem) 0
+            let fp = mlcm.Confusion.Column(labelIndex) |> Seq.foldi (fun rI acc elem -> if rI = labelIndex then acc else acc + elem) 0
+            let fn = mlcm.Confusion.Row(labelIndex) |> Seq.foldi (fun cI acc elem -> if cI = labelIndex then acc else acc + elem) 0
+            label, BinaryConfusionMatrix.create(tp, tn, fp, fn)
+        )
