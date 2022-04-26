e5e257bf2f4df4c41fe7ac0a3770f5a30e2bd32c
Andrew Abok <andyabok7@yahoo.com>
2022-04-26 15:45:13 +0300

    Revert "Fix breaking tests interpolation tests."
    This reverts commit 4c979ad992ae9fda2eb7392cba2dd3d3d5a220e9.

--- tests/FSharp.Stats.Tests/Interpolation.fs
+++ tests/FSharp.Stats.Tests/Interpolation.fs
@@ -7,10 +7,13 @@ open TestExtensions
 
 [<Tests>]
 let cubicInterpolationTests =
-    let t = vector [0.0; 1.0; 2.0; 3.0]
-    let y = vector [187.6;185.7;193.7;197.0]
+    let t = vector [-2.0; -1.0; 0.0; 1.0; 2.0]
+    let y = vector [1.0; 2.0; -1.0; 0.0; 1.0 ]
     let tt = vector [0.0;0.25;0.5;0.75;1.;1.25;1.5;1.75;2.;2.25;2.5;2.75;3.0]
     
+    let u = vector [1.0 ;4.0; 9.0; 16.0]
+    let t2 = vector [1.0; 2.0; 3.0; 4.0]
+
     testList "Interpolation.CubicSpline" [
         testCase "Natural Cubic Spline" <| fun () -> 
             //Verifies that the fitted point match the expectred fittied points
@@ -19,16 +22,14 @@ let cubicInterpolationTests =
                 CubicSpline.Simple.coefficients CubicSpline.Simple.Natural t y            
             let fitOutPut = tt |> Vector.map (CubicSpline.Simple.fit coefficientsSpline t)
             let expectedValues = vector [187.6; 186.4328125; 185.5425; 185.2059375; 185.7; 187.179375;189.31; 191.635625; 193.7; 195.1528125; 196.0675; 196.6234375;197.0]
-            TestExtensions.sequenceEqual Accuracy.high expectedValues fitOutPut "Fitted Values and Expected Output should be equal (double precision)"
+            TestExtensions.sequenceEqual Accuracy.low expectedValues fitOutPut "Fitted Values and Expected Output should be equal (double precision)"
     
-        let u = vector [1.0 ;4.0; 9.0; 16.0]
-        let t2 = vector [1.0; 2.0; 3.0; 4.0]
         testCase "Quadratic Cubic Spline" <| fun () ->            
             let coefficientsQuadraticSpline = 
                 CubicSpline.Simple.coefficients CubicSpline.Simple.Quadratic t2 u         
             let fittingFunc x = 
-                CubicSpline.Simple.fit coefficientsQuadraticSpline t2 x           
-            Expect.floatClose Accuracy.low (fittingFunc 1.5) 2.25  "Fitted Value should be equal (double precision)"
+                CubicSpline.Simple.fit coefficientsQuadraticSpline t x           
+            Expect.floatClose Accuracy.low (fittingFunc 1.5) 0.0  "Fitted Value should be equal (double precision)"
             Expect.floatClose Accuracy.low (fittingFunc 2.5) 6.25 "Fitted Value should be equal (double precision)"
             Expect.floatClose Accuracy.low (fittingFunc 3.5) 12.25 "Fitted Value  should be equal (double precision)"  
 
