c3a98d412c2f58d992d5ea0b46995b9092bae86e
muehlhaus <muehlhaus@bio.uni-kl.de>
2023-03-25 20:39:47 +0100

    Add Nelder-Mead

---
+++ src/FSharp.Stats/Optimization/NelderMead.fs
@@ -0,0 +1,263 @@
+﻿namespace FSharp.Stats.Optimization
+
+module NelderMead =
+    
+    open FSharp.Stats
+    open FSharp.Stats.Algebra
+    open FSharp.Stats.Algebra.LinearAlgebra
+
+    type NmConfig = {
+        ///<summary> Delta used to generate initial simplex for non-zero value elements </summary>
+        Delta  : float
+        ZDelta : float
+        ///<summary> Coefficient of reflection (Rho) </summary>  
+        Rho    : float
+        ///<summary> Coefficient of expansion (Chi) </summary>
+        Chi    : float 
+        ///<summary> Coefficient of contraction (Psi) </summary>
+        Psi    : float
+        ///<summary> Coefficient of shrinkage (Sigma) </summary>
+        Sigma  : float
+    }
+      with 
+        static member defaultInit () =
+            {
+                Delta = 0.05
+                ZDelta = 0.00025
+                Rho   = 1.
+                Chi   = 2.
+                Psi   = 0.5
+                Sigma = 0.5
+            }
+
+
+    type StepType = 
+      | Initialization
+      | Reflection
+      | Expansion
+      | OutsideContraction
+      | InsideContraction
+      | Shrink
+
+
+
+    type NmInterationResult = {
+      Simplexes      : vector []
+      Fx             : float array
+      Vectors        : vector []
+      Values         : float array
+      GradientNorms  : float array
+      SolutionVector : vector
+      Solution       : float
+      LastStep       : StepType
+    }
+
+
+
+    ///<summary> Create an initial simplex </summary>
+    let createSimplex (nmc : NmConfig) (x : vector) =    
+          let n = x.Length
+          let simplex =
+            Array.init (n+1) 
+                (fun _ -> Vector.copy x )      
+          for i = 1 to n do                      
+            if (x[i - 1] <> 0) then        
+              simplex[i][i - 1] <- (1. + nmc.Delta) * x[i - 1]
+            else        
+              simplex[i][i - 1] <- nmc.ZDelta        
+      
+          simplex
+
+
+    // determine the values of each vertice in the initial simplex
+    let rankVertices (simplexes : vector[]) (fn : vector -> float) = 
+      let x  = Array.copy simplexes  
+      let fx = simplexes |> Array.map fn
+      System.Array.Sort(fx,x)
+      (x,fx)
+      
+
+
+    let update (nmc : NmConfig) (stopCounter:OptimizationStop.StopCounter) (fn : vector -> float) (nmInterResult : NmInterationResult) =   
+      let simplexes = Array.copy  nmInterResult.Simplexes // Copy necessary ???
+      let fx =  nmInterResult.Fx 
+      let mutable laststep_ = nmInterResult.LastStep
+  
+      // Calculate centroid of n best points (ie excluding worst point) 
+      let xbar = 
+        let mutable tmp = Vector.zeroCreate simplexes[0].Length 
+        for i = 0 to (simplexes.Length - 2) do
+          tmp <- tmp + simplexes[i]
+    
+        let d = float (simplexes.Length - 1)    
+        tmp |> Vector.map (fun v -> v / d)
+
+      // Calculate reflection point
+      let xr = (1. + nmc.Rho) * xbar - nmc.Rho * simplexes[simplexes.Length - 1]   
+      let fxr = fn xr
+      stopCounter.IncFunctionEvaluationCounter()
+
+      if (fxr < fx[simplexes.Length - 2]) then  
+        // reflection point is better than worst point
+        if (fxr < fx[0]) then    
+          //reflection point is better than best point - > expand
+          let xe = (1. + nmc.Rho * nmc.Chi) * xbar - nmc.Rho * nmc.Chi * simplexes[simplexes.Length - 1]
+          let fxe = fn xe
+          stopCounter.IncFunctionEvaluationCounter()
+          if (fxe < fxr) then
+            // accept expansion point
+            simplexes[simplexes.Length - 1] <- xe
+            fx[simplexes.Length - 1] <- fxe
+            laststep_ <- StepType.Expansion
+      
+          else      
+            // accept reflection point
+            simplexes[simplexes.Length - 1] <- xr
+            fx[simplexes.Length - 1] <- fxr
+            laststep_ <- StepType.Reflection
+          
+        else
+          // accept reflection point
+          simplexes[simplexes.Length - 1] <- xr
+          fx[simplexes.Length - 1] <- fxr
+          laststep_ <- StepType.Reflection
+
+      else
+        // Try a contraction
+        if ((fx[simplexes.Length - 2] <= fxr) && (fxr < fx[simplexes.Length - 1])) then
+    
+          // perform an outside contraction
+          let xc = (1. + nmc.Psi * nmc.Rho) * xbar - nmc.Psi * nmc.Rho * simplexes[simplexes.Length - 1]
+          let fxc = fn xc
+          stopCounter.IncFunctionEvaluationCounter()
+          if (fxc < fxr) then
+            // accept the outside contraction
+            simplexes[simplexes.Length - 1] <- xc
+            fx[simplexes.Length - 1] <- fxc
+            laststep_ <- StepType.OutsideContraction      
+          else      
+            // perform a shrink step
+            for i = 1 to (simplexes.Length - 1) do        
+              simplexes[i] <- simplexes[0] + nmc.Sigma * (simplexes[i] - simplexes[0])
+              fx[i] <- fn simplexes[i]
+              stopCounter.IncFunctionEvaluationCounter()
+      
+            laststep_ <- StepType.Shrink 
+
+        else    
+          // perform an inside contraction
+          let xcc = (1. - nmc.Psi) * xbar + nmc.Psi * simplexes[simplexes.Length - 1];
+          let fxcc = fn xcc
+          stopCounter.IncFunctionEvaluationCounter()
+
+          if (fxcc < fx[simplexes.Length - 1]) then      
+            // accept inside contraction
+            simplexes[simplexes.Length - 1] <- xcc
+            fx[simplexes.Length - 1] <- fxcc
+            laststep_ <- StepType.InsideContraction
+
+          else
+            // perform a shrink step
+            for i = 1 to (simplexes.Length - 1) do
+
+              simplexes[i] <- simplexes[0] + nmc.Sigma * (simplexes[i] - simplexes[0])
+              fx[i] <- fn simplexes[i]
+              stopCounter.IncFunctionEvaluationCounter()
+
+            laststep_ <- StepType.Shrink
+  
+      let x,fx = rankVertices simplexes fn    
+      let evalFn = fx[0] //fn simplexes[0]
+
+      {
+        Simplexes = x
+        Fx        = fx
+        Vectors   = 
+            let tmp = nmInterResult.Vectors
+            tmp[stopCounter.IterationCounter] <- x[0] 
+            tmp
+        Values   = 
+            let tmp = nmInterResult.Values
+            tmp[stopCounter.IterationCounter] <- evalFn //fn simplexes[0] 
+            tmp
+        GradientNorms =
+            let tmp = nmInterResult.GradientNorms
+            tmp[stopCounter.IterationCounter] <- 
+                      2.0 * System.Math.Abs(fx[x.Length - 1] - fx[0]) /
+                      (System.Math.Abs(fx[x.Length - 1]) + System.Math.Abs(fx[0]) + System.Double.Epsilon)
+            tmp
+        //Counter = nmIterResult.Counter + 1 
+        SolutionVector = x[0]
+        Solution = evalFn
+        LastStep = laststep_
+      }
+
+
+
+    ///<summary> Initialize the optimization method </summary>
+    ///<remarks> The use of this function is intended for testing/debugging purposes only </remarks>
+    let initInteration (maxIteration:int) (initialsimplex :vector[]) (fn : vector -> float) = 
+  
+        let x,fx   = rankVertices initialsimplex fn      
+        let evalFn = fn initialsimplex[0]
+
+        {
+          Simplexes = x
+          Fx        = fx
+          Vectors   = 
+              let tmp = Array.zeroCreate (maxIteration+1) 
+              tmp[0] <- initialsimplex[0] 
+              tmp
+          Values   = 
+              let tmp = Array.zeroCreate (maxIteration+1) 
+              tmp[0] <- evalFn //fn initialsimplex[0] 
+              tmp
+          GradientNorms =
+              let tmp = Array.zeroCreate (maxIteration+1)
+              tmp[0] <- 2.0 * System.Math.Abs(fx[x.Length - 1] - fx[0]) /
+                        (System.Math.Abs(fx[x.Length - 1]) + System.Math.Abs(fx[0]) + System.Double.Epsilon)
+              tmp
+          //Counter  = 0 
+          SolutionVector = x[0]
+          Solution = evalFn
+          LastStep = StepType.Initialization
+        }
+
+ 
+
+    ///<summary> Minimize the given cost function and stop criteria </summary>
+    let minimizeWithStopCriteria (nmc : NmConfig) (x :vector) (fn : vector -> float) (stopCriteria:OptimizationStop.StopCriteria) = 
+      // Action<double> newMinimalValueFound)
+
+      let initialsimplex = createSimplex nmc x
+      //let stopCriteria = Stop.defaultStopCriteria
+      let stopCounter = OptimizationStop.StopCounter() 
+
+      //let minCostSoFar = iteration.Fx[0]
+
+      // Iterate the optimization method
+      let rec loop nmInterResult =
+        stopCounter.IncIerationCounter()
+        let newNmInterResult = update nmc stopCounter fn nmInterResult
+
+        printfn "Counter: %i" stopCounter.IterationCounter
+
+        // if (fx[0] < minCostSoFar) then
+        //   minCostSoFar = fx[0];
+        //   newMinimalValueFound?.Invoke(minCostSoFar);
+        let test = 
+          (not <| stopCriteria.CancellationToken.IsCancellationRequested && not <| System.Double.IsNaN(nmInterResult.Fx[0])) 
+          && stopCriteria.IsCriteria stopCounter  (newNmInterResult.Values[stopCounter.IterationCounter - 1]) (newNmInterResult.Values[stopCounter.IterationCounter])
+
+        //iterationValues_[endCriteria_.iterationCounter]
+        if test then
+          loop newNmInterResult      
+        else
+          { newNmInterResult with Solution = newNmInterResult.Values[stopCounter.IterationCounter] } 
+
+      loop (initInteration stopCriteria.MaxIteration initialsimplex fn)
+
+
+    ///<summary> Minimize the given cost function and stop criteria </summary>
+    let minimize (nmc : NmConfig) (x :vector) (fn : vector -> float) =
+      minimizeWithStopCriteria nmc x fn OptimizationStop.defaultStopCriteria
---
+++ src/FSharp.Stats/Optimization/OptimizationStop.fs
@@ -0,0 +1,301 @@
+﻿namespace FSharp.Stats.Optimization
+
+module OptimizationStop =
+
+  open System
+  
+  ///<summary>Possible criteria to end optimization</summary>
+  ///<remarks>Optimization can end because the maximum number of iterations has been reached,
+  ///a stationary point has been reached, or a stationary gradient has been reached</remarks>
+  [<Flags>] 
+  type StopCriteriaType =
+    | None                       = 0 
+    | MaximumIteration           = 1
+    | MaximumFunctionEvaluation  = 2
+    | MaximumGradientEvaluation  = 4
+    | MaximumHessianEvaluation   = 8
+    | StationaryPoint            = 16
+    | StationaryGradient         = 32
+    | StationaryHessian          = 64
+    | FunctionEpsilon            = 128
+    | GradientEpsilon            = 256 
+    | HessianEpsilon             = 512
+
+  type StopCounter(
+      iterationCounter                    : int,
+      functionEvaluationCounter           : int,
+      gradientEvaluationCounter           : int,
+      hessianEvaluationCounter            : int,
+      stationaryPointIterationsCounter    : int,
+      stationaryGradientIterationsCounter : int,
+      stationaryHessianIterationsCounter  : int,
+      endCriteria : StopCriteriaType
+      ) =
+    
+    let mutable iterationCounter                    = iterationCounter
+    let mutable functionEvaluationCounter           = functionEvaluationCounter
+    let mutable gradientEvaluationCounter           = gradientEvaluationCounter
+    let mutable hessianEvaluationCounter            = hessianEvaluationCounter
+    let mutable stationaryPointIterationsCounter    = stationaryPointIterationsCounter
+    let mutable stationaryGradientIterationsCounter = stationaryGradientIterationsCounter
+    let mutable stationaryHessianIterationsCounter  = stationaryHessianIterationsCounter
+    let mutable endCriteria                         = endCriteria
+
+    new () = StopCounter(0,0,0,0,0,0,0,StopCriteriaType.None)
+
+    with
+      member __.IterationCounter 
+        with get ()      = iterationCounter
+        and  set (value) = iterationCounter <- value
+      member __.IncIerationCounter() = 
+        iterationCounter <- iterationCounter + 1 
+
+      member __.FunctionEvaluationCounter
+        with get ()      = functionEvaluationCounter
+        and  set (value) = functionEvaluationCounter <- value
+      member __.IncFunctionEvaluationCounter() = 
+        functionEvaluationCounter <- functionEvaluationCounter + 1 
+
+      member __.GradientEvaluationCounter
+        with get ()      = gradientEvaluationCounter
+        and  set (value) = gradientEvaluationCounter <- value
+      member __.IncGradientEvaluationCounter() = 
+        gradientEvaluationCounter <- gradientEvaluationCounter + 1 
+
+      member __.HessianEvaluationCounter
+        with get ()      = hessianEvaluationCounter
+        and  set (value) = hessianEvaluationCounter <- value
+      member __.IncHessianEvaluationCounter() = 
+        hessianEvaluationCounter <- hessianEvaluationCounter + 1 
+
+      member __.StationaryPointIterationsCounter
+        with get ()      = stationaryPointIterationsCounter
+        and  set (value) = stationaryPointIterationsCounter <- value
+      member __.IncStationaryPointIterationsCounter() = 
+        stationaryPointIterationsCounter <- stationaryPointIterationsCounter + 1 
+
+      member __.StationaryGradientIterationsCounter
+        with get ()      = stationaryGradientIterationsCounter
+        and  set (value) = stationaryGradientIterationsCounter <- value
+      member __.IncStationaryGradientIterationsCounter() = 
+        stationaryGradientIterationsCounter <- stationaryGradientIterationsCounter + 1 
+
+      member __.StationaryHessianIterationsCounter
+        with get ()      = stationaryHessianIterationsCounter
+        and  set (value) = stationaryHessianIterationsCounter <- value 
+      member __.IncStationaryHessianIterationsCounter() = 
+        stationaryHessianIterationsCounter <- stationaryHessianIterationsCounter + 1 
+
+      member __.EndCriteria
+        with get ()      = endCriteria
+        and  set (value) = endCriteria <- value
+      member __.AddEndCriteriaFlag(flag:StopCriteriaType) =
+        endCriteria <- endCriteria ||| flag
+
+      member __.Reset() =
+        iterationCounter <- 0
+        functionEvaluationCounter <- 0
+        gradientEvaluationCounter <- 0
+        hessianEvaluationCounter <- 0
+        stationaryPointIterationsCounter <- 0
+        stationaryGradientIterationsCounter <- 0
+        stationaryHessianIterationsCounter <- 0
+        endCriteria <- StopCriteriaType.None
+
+  let initStopCounter() = new StopCounter()
+
+  type StopCriteria = {
+    MaxIteration                    : int
+    MaxFunctionEvaluation           : int
+    MaxGradientEvaluation           : int
+    MaxHessianEvaluation            : int
+    MaxStationaryPointIterations    : int
+    MaxStationaryGradientIterations : int
+    MaxStationaryHessianIterations  : int
+    MinFunctionEpsilon              : float
+    MinGradientEpsilon              : float
+    MinHessianEpsilon               : float
+    CancellationToken               : System.Threading.CancellationToken
+  } 
+  with
+    
+    static member InitWith (maxiteration, epsilon, maxfunctionevaluation, maxstationarypointiterations,cancellationToken) =
+      {
+        MaxIteration                    = maxiteration
+        MaxFunctionEvaluation           = maxfunctionevaluation
+        MaxGradientEvaluation           = maxfunctionevaluation
+        MaxHessianEvaluation            = maxfunctionevaluation
+        MaxStationaryPointIterations    = maxstationarypointiterations
+        MaxStationaryGradientIterations = maxstationarypointiterations
+        MaxStationaryHessianIterations  = maxstationarypointiterations
+        MinFunctionEpsilon              = epsilon 
+        MinGradientEpsilon              = epsilon 
+        MinHessianEpsilon               = epsilon
+        CancellationToken               = cancellationToken         
+      }
+
+    ///<summary>Check if the iteration number is less than the maximum iteration</summary>
+    ///<remarks>
+    ///If iteration count is equal to or greater than the maximum number of iterations then
+    ///the ending criteria is set to <c>CriteriaType.MaximumIteration</c> and the function returns true.
+    ///</remarks>    
+    member __.CheckIteration (counter:StopCounter) =
+      if counter.IterationCounter >= __.MaxIteration then
+        StopCriteriaType.MaximumIteration
+      else
+        StopCriteriaType.None
+
+    ///<summary>Check if the number of function evaluations is less than the maximum </summary>
+    ///<remarks>
+    ///If the number of function evaluations is equal to or greater than the maximum number of
+    ///function evaluations then the ending criteria is set to <c>CriteriaType.MaximumFunctionEvaluation</c>
+    /// and the function returns true.
+    ///</remarks>
+    member __.CheckFunctionEvaluations (counter:StopCounter) =
+      if (counter.FunctionEvaluationCounter >= __.MaxFunctionEvaluation) then
+        StopCriteriaType.MaximumFunctionEvaluation
+      else
+        StopCriteriaType.None
+
+    ///<summary>Check if the number of gradient evaluations is less than the maximum </summary>
+    ///<remarks>
+    ///If the number of gradient evaluations is equal to or greater than the maximum number of
+    ///gradient evaluations then the ending criteria is set to <c>CriteriaType.MaximumGradientEvaluation</c>
+    /// and the function returns true.
+    ///</remarks>
+    member __.CheckGradientEvaluations (counter:StopCounter) =
+      if (counter.GradientEvaluationCounter >= __.MaxGradientEvaluation) then
+        StopCriteriaType.MaximumGradientEvaluation
+      else
+        StopCriteriaType.None
+
+    ///<summary>Check if the number of hessian evaluations is less than the maximum </summary>
+    ///<remarks>
+    ///If the number of hessian evaluations is equal to or greater than the maximum number of
+    ///hessian evaluations then the ending criteria is set to <c>CriteriaType.MaximumHessianEvaluation</c>
+    /// and the function returns true.
+    ///</remarks>
+    member __.CheckHessianEvaluations (counter:StopCounter) =
+      if (counter.HessianEvaluationCounter >= __.MaxHessianEvaluation) then
+        StopCriteriaType.MaximumHessianEvaluation
+      else
+        StopCriteriaType.None
+
+    ///<summary>Check if objective function changed by less than the function epsilon</summary>
+    ///<remarks>
+    /// If the change in objective function is less than the function epsilon then a possible stationary
+    /// point has been found.  If the number of repeated iterations at this possible stationary point is
+    /// greater than the maximum iterations at a station point then the ending criteria is set to
+    /// <c>CriteriaType.StationaryPoint</c> and the function returns true;
+    ///</remarks>
+    member __.CheckStationaryPoint (stopCounter:StopCounter) (fold:float) (fnew:float) =
+    
+      let test = not ((System.Math.Abs(fold - fnew) >= __.MinFunctionEpsilon))
+      if test then
+        stopCounter.IncStationaryPointIterationsCounter()
+      elif (stopCounter.StationaryPointIterationsCounter <> 0) then
+        stopCounter.StationaryPointIterationsCounter <- 0
+      
+      if test && (stopCounter.StationaryPointIterationsCounter > __.MaxStationaryPointIterations) then
+        StopCriteriaType.StationaryPoint
+      else
+        StopCriteriaType.None
+      
+    ///<summary>Check if gradient function changed by less than the gradient epsilon</summary>
+    ///<remarks>
+    /// If the change in gradient function is less than the gradient epsilon then a possible stationary
+    /// point has been found.  If the number of repeated iterations at this possible stationary point is
+    /// greater than the maximum iterations at a station point then the ending criteria is set to
+    /// <c>CriteriaType.StationaryPoint</c> and the function returns true;
+    ///</remarks>
+    member __.CheckStationaryGradient (stopCounter:StopCounter) (gold:float) (gnew:float) =
+      let test = (System.Math.Abs(gold - gnew) < __.MinGradientEpsilon)
+      if (test) then
+        stopCounter.IncStationaryGradientIterationsCounter()
+      elif (stopCounter.StationaryGradientIterationsCounter <> 0) then
+        stopCounter.StationaryPointIterationsCounter <- 0
+      
+      if (test && (stopCounter.StationaryGradientIterationsCounter > __.MaxStationaryGradientIterations)) then
+        StopCriteriaType.StationaryGradient
+      else
+        StopCriteriaType.None
+
+    ///<summary>Check if hessian function changed by less than the hessian epsilon</summary>
+    ///<remarks>
+    /// If the change in hessian function is less than the hessian epsilon then a possible stationary
+    /// point has been found.  If the number of repeated iterations at this possible stationary point is
+    /// greater than the maximum iterations at a station point then the ending criteria is set to
+    /// <c>CriteriaType.StationaryPoint</c> and the function returns true;
+    ///</remarks>
+    member __.CheckStationaryHessian (stopCounter:StopCounter) (gold:float) (gnew:float) =
+      let test = (System.Math.Abs(gold - gnew) < __.MinHessianEpsilon)
+      if test then
+        stopCounter.IncStationaryHessianIterationsCounter()
+      elif (stopCounter.StationaryHessianIterationsCounter <> 0) then
+        stopCounter.StationaryPointIterationsCounter <- 0
+      if (test && (stopCounter.StationaryHessianIterationsCounter > __.MaxStationaryHessianIterations)) then
+        StopCriteriaType.StationaryHessian
+      else
+        StopCriteriaType.None
+
+    ///<summary>Check if objective function value is less than the function epsilon</summary>
+    ///<remarks>
+    /// If the objective function value is less than the function epsilon and only positive optimization
+    /// is allowed then the ending criteria is set to <c>CriteriaType.FunctionEpsilon</c> and the
+    /// function returns true;
+    ///</remarks>
+    member __.CheckFunctionEpsilon (v) =
+      if (v < __.MinFunctionEpsilon) then
+        StopCriteriaType.FunctionEpsilon
+      else 
+        StopCriteriaType.None
+
+    ///<summary>Check if the norm of the gradient is less than the gradient epsilon</summary>
+    ///<remarks>
+    /// If the norm of the gradient is less than the gradient epsilon then the ending criteria is set
+    /// to <c>CriteriaType.GradientEpsilon</c> and the function returns true;
+    ///</remarks>
+    member __.CheckGradientEpsilon (normDiff) =
+    
+      if (normDiff < __.MinGradientEpsilon) then
+        StopCriteriaType.GradientEpsilon
+      else
+        StopCriteriaType.None
+    
+
+    ///<summary>Check if the norm of the hessian is less than the hessian epsilon</summary>
+    ///<remarks>
+    /// If the norm of the hessian is less than the gradient epsilon then the ending criteria is set
+    /// to <c>CriteriaType.HessianEpsilon</c> and the function returns true;
+    ///</remarks>
+    member __.CheckHessianEpsilon (normDiff) =
+      if (normDiff < __.MinHessianEpsilon) then
+        StopCriteriaType.HessianEpsilon
+      else
+        StopCriteriaType.None
+
+
+    ///<summary>Check if ending criteria are met</summary>
+    ///<remarks>Returns true if one of the ending criteria is met, otherwise it returns false</remarks>
+    member __.IsCriteria (stopCounter:StopCounter) (fold : float) (fnew:float) =
+      let testFlags =
+        __.CheckIteration stopCounter |||
+        __.CheckFunctionEvaluations stopCounter |||
+        __.CheckStationaryPoint stopCounter fold fnew |||
+        __.CheckFunctionEpsilon fnew |||
+        __.CheckFunctionEpsilon fold
+      stopCounter.EndCriteria <- testFlags
+      testFlags = StopCriteriaType.None   
+
+    ///<summary>Check if gradient criteria are met</summary>
+    ///<remarks>Returns true if one of the gradient criteria is not met, otherwise it returns false</remarks>
+    member __.IsGradientCriteria (stopCounter:StopCounter) (normgold:float) (normgnew:float) =
+      let testFlags =
+        __.CheckGradientEvaluations stopCounter |||
+        __.CheckStationaryGradient stopCounter normgnew normgold |||
+        __.CheckGradientEpsilon normgnew |||
+        __.CheckGradientEpsilon normgold
+      stopCounter.EndCriteria <- testFlags
+      testFlags = StopCriteriaType.None
+
+  let defaultStopCriteria = StopCriteria.InitWith (1000, 1e-8, 10000, 100, System.Threading.CancellationToken.None)
