b0c973ac4e49dfacc3b5fc8d41b71d182ce2a84f
Kevin Malenfant <kevin@xaai.ch>
2024-04-23 20:22:15 -0600

    update Random docs

--- src/FSharp.Stats/Random.fs
+++ src/FSharp.Stats/Random.fs
@@ -1,73 +1,75 @@
 ﻿namespace FSharp.Stats
 
-/// Uniform random number generators
+/// <summary>Uniform random number generators</summary>
 module Random = 
     open System.Threading
     open System
 
-    /// Interface that every uniform random number generator must implement.
+    /// <summary>Interface that every uniform random number generator must implement.</summary>
     type IRandom = interface
+        /// <summary>Returns a non-negative random integer.</summary>
+        /// <returns>A 32-bit signed integer that is greater than or equal to 0 and less than <see cref="System.Int32.MaxValue"/>.</returns>
         abstract NextInt : unit -> int
-        abstract NextInt : int -> int
+        /// <summary>Returns a non-negative random integer that is less than the specified maximum.</summary>
+        /// <param name="maxValue">The exclusive upper bound of the random number to be generated. <paramref name="maxValue"/> must be greater than or equal to 0.</param>
+        /// <returns>A 32-bit signed integer that is greater than or equal to 0, and less than <paramref name="maxValue"/>; that is, the range of return values ordinarily includes 0 but not <paramref name="maxValue"/>. However, if <paramref name="maxValue"/> equals 0, <paramref name="maxValue"/> is returned.</returns>
+        abstract NextInt : maxValue:int -> int
+        /// <summary>Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0.</summary>
+        /// <returns>A double-precision floating point number that is greater than or equal to 0.0, and less than 1.0.</returns>
         abstract NextFloat : unit -> float
     end
 
-    ///// A standard implementation of a uniform random source using System.Random()
+    /// <summary>A standard implementation of a uniform random source using <see cref="System.Random"/> that is thread-safe.</summary>
     type RandThreadSafe =
         val mutable rnd : ThreadLocal<Random>
-        /// Constructs the default random number generator using a time dependent default seed value.
-        new() = { rnd = new ThreadLocal<Random>(fun () -> new Random()) }
-        /// If n is negative, the random number generator seed is based on system time, if it is zero or positive it will
-        /// use n as the seed.
-        new(n) as this = { rnd = new ThreadLocal<Random>(fun () -> new Random()) }
+        /// <summary>Constructs the default random number generator using a time dependent default seed value.</summary>
+        new() = { rnd = new ThreadLocal<Random>(fun () -> Random()) }
+        /// <summary>Constructs a random number generator with the specified seed value.</summary>
+        /// <param name="n">If n is negative, the random number generator seed is based on system time, if it is zero or positive it will
+        /// use n as the seed.</param>
+        new(n) as this = { rnd = new ThreadLocal<Random>(fun () -> Random()) }
                          then
-                            if n >= 0 then this.rnd <- new ThreadLocal<Random>(fun () -> new Random(n))
+                            if n >= 0 then this.rnd <- new ThreadLocal<Random>(fun () -> Random(n))
         interface IRandom with
             member x.NextInt() = x.rnd.Value.Next()
-            ///maxValue is not part of the possible sampling range (minVal &lt;= x &lt; maxVal)
             member x.NextInt maxValue = x.rnd.Value.Next(maxValue)
-            /// Returns a random floating-point number that is greater or equal to 0.0, and less then 1.0
             member x.NextFloat() =x.rnd.Value.NextDouble()
-        end
 
+    /// <summary>A basic implementation of a uniform random source using <see cref="System.Random"/> that is not thread-safe.</summary>
     type RandBasic =
         val mutable rnd : Random
-        /// Constructs the default random number generator using a time dependent default seed value.
-        new() = { rnd = new Random() }
-        /// If n is negative, the random number generator seed is based on system time, if it is zero or positive it will
-        /// use n as the seed.
-        new(n) as this = { rnd = new Random() }
+        /// <summary>Constructs the default random number generator using a time dependent default seed value.</summary>
+        new() = { rnd = Random() }
+        /// <summary>Constructs a random number generator with the specified seed value.</summary>
+        /// <param name="n">If n is negative, the random number generator seed is based on system time, if it is zero or positive it will
+        /// use n as the seed.</param>
+        new(n) as this = { rnd = Random() }
                          then
                             if n >= 0 then this.rnd <- new Random(n)
         interface IRandom with
-            ///maxValue is not part of the possible sampling range (minVal &lt;= x &lt; maxVal)
             member x.NextInt() = x.rnd.Next()
             member x.NextInt maxValue = x.rnd.Next(maxValue)
             member x.NextFloat() =x.rnd.NextDouble()
-        end
 
+        
     /// <summary>The uniform random source used for sampling functions.</summary>
-    /// <remarks></remarks>
-    /// <param name="rndgen"></param>
-    /// <returns></returns>
-    /// <example>
-    /// <code>
-    /// </code>
-    /// </example>
-    let mutable rndgen = new RandThreadSafe() :> IRandom
+    /// <remarks>Defaults to an instance of <see cref="RandThreadSafe"/>.</remarks>
+    let mutable rndgen = RandThreadSafe() :> IRandom
+
     /// <summary>Sets the random number generator used for sampling.</summary>
-    /// <remarks></remarks>
-    /// <param name="rg"></param>
-    /// <returns></returns>
-    /// <example>
-    /// <code>
-    /// </code>
-    /// </example>
+    /// <param name="rg">The random number generator to use for sampling.</param>
     let SetSampleGenerator rg = rndgen <- rg
-    /// Returns the random number generator used for sampling.
+    
+    /// <summary>Returns the random number generator used for sampling.</summary>
+    /// <returns>The current random number generator used for sampling.</returns>
     let GetSampleGenerator () = rndgen
 
-    /// random number sampling method for pairs of independent, standard, normally distributed random numbers
+    /// <summary>Generates a pair of independent, standard, normally distributed random numbers using the Box-Muller transform.</summary>
+    /// <returns>A tuple of two independent, standard, normally distributed random numbers.</returns>
+    /// <remarks>
+    /// This method uses the current random number generator to generate two uniform random numbers, and then applies the 
+    /// Box-Muller transform to convert them into a pair of independent, standard, normally distributed random numbers.
+    /// </remarks>
     let boxMullerTransform() =
         let (u1,u2) = rndgen.NextFloat(),rndgen.NextFloat()
         let z0 = sqrt(-2. * log u1) * cos (2. * pi * u2)
