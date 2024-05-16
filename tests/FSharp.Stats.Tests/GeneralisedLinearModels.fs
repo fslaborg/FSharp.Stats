4d03d46f9f3c6bf266c90ec31de75167732001db
Christopher Lux <lux-christopher@web.de>
2024-05-16 13:15:12 +0200

    Add tests for the variance of Inv.Gaussian Family

--- tests/FSharp.Stats.Tests/GeneralisedLinearModels.fs
+++ tests/FSharp.Stats.Tests/GeneralisedLinearModels.fs
@@ -556,6 +556,27 @@ let familyVarianceFunctions =
                         expected
                         actual
                         $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+     
+        testCase "Inv.Gaussian" <| fun () ->
+            let formular (x:float) = x**3
+            let expected    = Array.map formular testingArray
+            let actualFormular =  GlmDistributionFamily.getVariance (GlmDistributionFamily.InverseGaussian)
+            let actual      = Array.map actualFormular testingArray
+            for i=0 to testingArray.Length-1 do
+                let expected    = expected.[i]
+                let actual      = actual.[i] 
+                if isInf actual then
+                    Expect.isTrue (isInf expected) $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+                elif isNegInf actual then
+                    Expect.isTrue (isNegInf expected) $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+                elif isNan actual then
+                    Expect.isTrue (isNan expected) $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+                else
+                    Expect.floatClose 
+                        Accuracy.high
+                        expected
+                        actual
+                        $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
 
     ]
 
