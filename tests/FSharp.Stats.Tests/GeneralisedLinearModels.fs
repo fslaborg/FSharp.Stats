148a933799c9e224aa5ad94d54a273746db62855
Christopher Lux <lux-christopher@web.de>
2024-05-16 10:42:32 +0200

    Add tests for the variance of Poisson Family

--- tests/FSharp.Stats.Tests/GeneralisedLinearModels.fs
+++ tests/FSharp.Stats.Tests/GeneralisedLinearModels.fs
@@ -514,6 +514,27 @@ let familyVarianceFunctions =
                         actual
                         $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
 
+        testCase "Poisson" <| fun () ->
+            let formular x = x
+            let expected    = Array.map formular testingArray
+            let actualFormular =  GlmDistributionFamily.getVariance (GlmDistributionFamily.Poisson)
+            let actual      = Array.map formular testingArray
+            for i=0 to testingArray.Length-1 do
+                let expected    = expected.[i]
+                let actual      = actual.[i] 
+                if isInf actual then
+                    Expect.isTrue (isInf expected) $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+                elif isNegInf actual then
+                    Expect.isTrue (isNegInf expected) $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+                elif isNan actual then
+                    Expect.isTrue (isNan expected) $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+                else
+                    Expect.floatClose 
+                        Accuracy.high
+                        expected
+                        actual
+                        $"Element {i} Variance function is incorrect. {testingArray.[i]} was linked to {actual} instead to {expected}"
+
 
     ]
 
