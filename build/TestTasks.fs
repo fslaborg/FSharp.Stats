c1ba0dd4434f256e285c0d7a7353d469d394901f
Kevin Schneider <schneider.kev@outlook.de>
2025-04-08 16:24:13 +0200

    update to .net 8 in preparation for INumerics core

--- build/TestTasks.fs
+++ build/TestTasks.fs
@@ -7,14 +7,16 @@ open ProjectInfo
 open BasicTasks
 
 let runTests = BuildTask.create "RunTests" [clean; build] {
-    Fake.DotNet.DotNet.test(fun testParams ->
-        {
-            testParams with
-                Logger = Some "console;verbosity=detailed"
-                Configuration = DotNet.BuildConfiguration.fromString configuration
-                NoBuild = true
+    testProject
+    |> Fake.DotNet.DotNet.test (fun testParams ->
+        { testParams with
+            Logger = Some "console;verbosity=detailed"
+            Configuration = DotNet.BuildConfiguration.fromString configuration
+            NoBuild = true
+            MSBuildParams = { testParams.MSBuildParams with DisableInternalBinLog = true }
         }
-    ) testProject
+        |> DotNet.Options.withCustomParams (Some "-tl")
+    )
 }
 
 // to do: use this once we have actual tests
@@ -25,6 +27,7 @@ let runTestsWithCodeCov = BuildTask.create "RunTestsWithCodeCov" [clean; build] 
             testParams with
                 MSBuildParams = {
                     standardParams with
+                        DisableInternalBinLog = true
                         Properties = [
                             "AltCover","true"
                             "AltCoverCobertura","../../codeCov.xml"
