eb3b9d281637a47b5cdac5d9fa01e0105286d318
Christopher Lux <59606965+LibraChris@users.noreply.github.com>
2025-06-26 16:14:51 +0200

    Refactor to support generic numeric type 'T

--- src/FSharp.Stats/SpecialFunctions/Beta.fs
+++ src/FSharp.Stats/SpecialFunctions/Beta.fs
@@ -4,13 +4,18 @@ open System
 open FSharp.Stats
 open FSharp.Stats.GenericMath
 
+// TODO: Generalize lowerIncompleteRegularized, lowerIncomplete, and powerSeries to support generic type 'T instead of using float
+
 /// The beta function B(p,q), or the beta integral (also called the Eulerian integral of the first kind) is defined by
 ///
 /// B(p, q) = (Γ(p) * Γ(q)) / Γ(p+q)
 module Beta =
 
-    let private EPS : 'T = T 3.0e-8    // Precision.DoublePrecision;
-    let private FPMIN : 'T = T 1.0e-30 // 0.0.Increment()/eps
+    // let private EPS : 'T = T 3.0e-8    // Precision.DoublePrecision;
+    // let private FPMIN : 'T = T 1.0e-30 // 0.0.Increment()/eps
+
+    let private f_EPS = T 3.0e-8    // Precision.DoublePrecision;
+    let private f_FPMIN = T 1.0e-30 // 0.0.Increment()/eps
 
     ///<summary>
     /// Computes an approximation of the real value of the log beta function using approximations for the gamma function using Lanczos Coefficients described in Numerical Recipes (Press et al) 
@@ -109,100 +114,59 @@ module Beta =
     /// <param name="a">The first Beta parameter, a positive real number.</param>
     /// <param name="b">The second Beta parameter, a positive real number.</param>
     /// <param name="x">The upper limit of the integral.</param>
-    let inline lowerIncompleteRegularized
-        (a: ^T when ^T :> Numerics.INumber<^T>
-              and ^T : (new: unit -> ^T)
-              and ^T : struct
-              and ^T : equality
-              and ^T :> ValueType
-              and ^T :> System.Numerics.IFloatingPoint<^T>
-              and ^T :> System.Numerics.IExponentialFunctions<^T>
-              and ^T :> System.Numerics.ILogarithmicFunctions<^T>
-              and ^T :> System.Numerics.IRootFunctions<^T>
-              and ^T :> System.Numerics.IPowerFunctions<^T>
-              and ^T : comparison)
-        (b: ^T when ^T :> Numerics.INumber<^T>
-              and ^T : (new: unit -> ^T)
-              and ^T : struct
-              and ^T : equality
-              and ^T :> ValueType
-              and ^T :> System.Numerics.IFloatingPoint<^T>
-              and ^T :> System.Numerics.IExponentialFunctions<^T>
-              and ^T :> System.Numerics.ILogarithmicFunctions<^T>
-              and ^T :> System.Numerics.IRootFunctions<^T>
-              and ^T :> System.Numerics.IPowerFunctions<^T>
-              and ^T : comparison)
-        (x: ^T when ^T :> Numerics.INumber<^T>
-              and ^T : (new: unit -> ^T)
-              and ^T : struct
-              and ^T : equality
-              and ^T :> ValueType
-              and ^T :> System.Numerics.IFloatingPoint<^T>
-              and ^T :> System.Numerics.IExponentialFunctions<^T>
-              and ^T :> System.Numerics.ILogarithmicFunctions<^T>
-              and ^T :> System.Numerics.IRootFunctions<^T>
-              and ^T :> System.Numerics.IPowerFunctions<^T>
-              and ^T : comparison) =       
-        if (a < T 0.0) then invalidArg "a" "Argument must not be negative"
-        if (b < T 0.0) then invalidArg "b" "Argument must not be negative"
-        if (x < T 0.0 || x > T 1.0) then invalidArg "x" "Argument XY interval is inclusive"
-        let bt: 'T = 
-            if (x = T 0.0 || x = T 1.0) then
-                T 0.0
+    let lowerIncompleteRegularized a b x =       
+        if (a < 0.0) then invalidArg "a" "Argument must not be negative"
+        if (b < 0.0) then invalidArg "b" "Argument must not be negative"
+        if (x < 0.0 || x > 1.0) then invalidArg "x" "Argument XY interval is inclusive"
+        let bt = 
+            if (x = 0.0 || x = 1.0) then
+                0.0
             else
-                exp (Gamma._gammaLn (a + b) - Gamma._gammaLn a - Gamma._gammaLn b + (a*log(x)) + (b*log(T 1.0 - x)))
+                exp (Gamma._gammaLn (a + b) - Gamma._gammaLn a - Gamma._gammaLn b + (a*Math.Log(x)) + (b*Math.Log(1.0 - x)))
 
-        let isSymmetryTransformation = ( x >= (a + T 1.0)/(a + b + T 2.0))
+        let isSymmetryTransformation = ( x >= (a + 1.0)/(a + b + 2.0))
 
-        let inline symmetryTransformation 
-            (a: 'T) (b: 'T) (x: 'T) =
+        let symmetryTransformation a b x =
             let qab = a + b
-            let qap = a + T 1.0
-            let qam = a - T 1.0
-            let c = T 1.0
-            let d: 'T = 
-                let tmp =  T 1.0 - (qab * x / qap)
-                if (abs tmp < FPMIN) then T 1. / FPMIN else T 1. / tmp
+            let qap = a + 1.0
+            let qam = a - 1.0
+            let c = 1.0
+            let d = 
+                let tmp =  1.0 - (qab * x / qap)
+                if (abs tmp < f_FPMIN) then 1. / f_FPMIN else 1. / tmp
             let h = d
-            let rec loop (m: 'T) (mm: 'T) (d: 'T) (h: 'T) (c: 'T) =                
-                let aa = m * (b - m)*x/((qam + mm)*(a + mm))
+            let rec loop m mm d h c =                
+                let aa = float m * (b - float m)*x/((qam + mm)*(a + mm))
                 let d' = 
-                    let tmp = T 1.0 + (aa*d)
-                    if (abs tmp < FPMIN) then T 1. / FPMIN else T 1. / tmp
+                    let tmp = 1.0 + (aa*d)
+                    if (abs tmp < f_FPMIN) then 1. / f_FPMIN else 1. / tmp
                 let c' = 
-                    let tmp = T 1.0 + (aa/c)
-                    if (abs tmp < FPMIN) then FPMIN else tmp
-                let h': 'T = h * d' * c'
-                let aa': 'T = -(a + m)*(qab + m)*x/((a + mm)*(qap + mm))
-                let d'': 'T = 
-                    let tmp = T 1.0 + (aa' * d')
-                    if (abs tmp < FPMIN) then T 1. / FPMIN else T 1. / tmp
-                let c'': 'T = 
-                    let tmp = T 1.0 + (aa'/c')
-                    if (abs tmp < FPMIN) then FPMIN else tmp
+                    let tmp = 1.0 + (aa/c)
+                    if (abs tmp < f_FPMIN) then f_FPMIN else tmp
+                let h' = h * d' * c'
+                let aa' = -(a + float m)*(qab + float m)*x/((a + mm)*(qap + mm))
+                let d'' = 
+                    let tmp = 1.0 + (aa' * d')
+                    if (abs tmp < f_FPMIN) then 1. / f_FPMIN else 1. / tmp
+                let c'' = 
+                    let tmp = 1.0 + (aa'/c')
+                    if (abs tmp < f_FPMIN) then f_FPMIN else tmp
                 
-                let del: 'T = d''*c''
-                let h'': 'T = h' * del
+                let del = d''*c''
+                let h'' = h' * del
                 
-                let tepmI: 'T = 
-                    bt*h''/a
-
-                if abs (del - T 1.0) <= EPS then
-                    
-                    if isSymmetryTransformation then 
-                        T 1.0 - tepmI 
-                    else 
-                        tepmI
+                if abs (del - 1.0) <= f_EPS then
+                     if isSymmetryTransformation then 1.0 - (bt*h''/a) else bt*h''/a
                 else
-                    if m < T 140 then
-                        loop (m+T 1) (mm+T 2.) d'' h'' c''
+                    if m < 140 then
+                        loop (m+1) (mm+2.) d'' h'' c''
                     else 
-                            if isSymmetryTransformation then T 1.0 - tepmI else tepmI
+                            if isSymmetryTransformation then 1.0 - (bt*h''/a) else bt*h''/a
                 
-            loop (T 1) (T 2.) d h c             
+            loop 1 2. d h c             
 
         if isSymmetryTransformation then
-            symmetryTransformation b a (T 1.0-x)
+            symmetryTransformation b a (1.0-x)
         else
             symmetryTransformation a b x
 
@@ -213,7 +177,7 @@ module Beta =
     /// <param name="a">The first Beta parameter, a positive real number.</param>
     /// <param name="b">The second Beta parameter, a positive real number.</param>
     /// <param name="x">The upper limit of the integral.</param>
-    let inline lowerIncomplete(a, b, x) : 'T=
+    let lowerIncomplete(a, b, x) =
         (lowerIncompleteRegularized a b x) * (beta a b)
  
  
@@ -221,11 +185,11 @@ module Beta =
     ///   Power series for incomplete beta integral. Use when b*x
     ///   is small and x not too close to 1.
     /// </summary>
-    let inline powerSeries a b x =
-        let ai = T 1.0 / a
-        let ui = (T 1.0 - b) * x 
-        let t1 = ui / (a + T 1.0)
-        let z  = Ops.epsilon * ai |> T
+    let powerSeries a b x =
+        let ai = 1.0 / a
+        let ui = (1.0 - b) * x 
+        let t1 = ui / (a + 1.0)
+        let z  = Ops.epsilon * ai
 
         let rec loop u t v s n =
             if (abs v > z) then 
